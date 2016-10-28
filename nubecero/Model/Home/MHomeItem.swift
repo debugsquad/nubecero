0bb0243c40d360805b94f94a36db4a49fd47b5dd
vauxhall <zero.griffin@gmail.com>
2016-10-28 17:01:47 -0500

    model home item init

--- nubecero/Model/Home/MHomeItem.swift
+++ nubecero/Model/Home/MHomeItem.swift
@@ -1,6 +1,21 @@
-import Foundation
+import UIKit
 
 class MHomeItem
 {
+    let reusableIdentifier:String
+    let cellHeight:CGFloat
+    let selectable:Bool
     
+    init(reusableIdentifier:String, cellHeight:CGFloat, selectable:Bool)
+    {
+        self.reusableIdentifier = reusableIdentifier
+        self.cellHeight = cellHeight
+        self.selectable = selectable
+    }
+    
+    //MARK: public
+    
+    func selected(controller:CHome)
+    {
+    }
 }

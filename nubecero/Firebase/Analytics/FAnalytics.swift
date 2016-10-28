2351e660b93c10092bb920abd91af8e36ea7db93
vauxhall <zero.griffin@gmail.com>
2016-10-28 10:00:52 -0500

    cleansed analytics

--- nubecero/Firebase/Analytics/FAnalytics.swift
+++ nubecero/Firebase/Analytics/FAnalytics.swift
@@ -4,10 +4,7 @@ import Firebase
 class FAnalytics
 {
     private let kEventScreen:NSString = "Screen"
-    private let kEventFilter:NSString = "Filter"
     private let kEventAction:NSString = "Action"
-    private let kEventActionShare:NSString = "Share"
-    private let kEventActionCrop:NSString = "Crop"
     
     //MARK: public
     
@@ -29,75 +26,6 @@ class FAnalytics
             FIRAnalytics.logEvent(
                 withName:kFIREventSelectContent,
                 parameters:parameters)
-                
-            #endif
-        }
-    }
-    
-    func next(filter:MFiltersItem)
-    {
-        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
-        {
-            let parameters:[String:NSObject] = [
-                kFIRParameterContentType:self.kEventFilter,
-                kFIRParameterItemID:filter.name as NSObject
-            ]
-            
-            #if DEBUG
-                
-                print(parameters)
-                
-            #else
-                
-                FIRAnalytics.logEvent(
-                    withName:kFIREventSelectContent,
-                    parameters:parameters)
-                
-            #endif
-        }
-    }
-    
-    func share()
-    {
-        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
-        {
-            let parameters:[String:NSObject] = [
-                kFIRParameterContentType:self.kEventAction,
-                kFIRParameterItemID:self.kEventActionShare
-            ]
-            
-            #if DEBUG
-            
-                print(parameters)
-                
-            #else
-                
-            FIRAnalytics.logEvent(
-                withName:kFIREventSelectContent,
-                parameters:parameters)
-
-            #endif
-        }
-    }
-    
-    func crop()
-    {
-        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
-        {
-            let parameters:[String:NSObject] = [
-                kFIRParameterContentType:self.kEventAction,
-                kFIRParameterItemID:self.kEventActionCrop
-            ]
-            
-            #if DEBUG
-                
-                print(parameters)
-                
-            #else
-                
-                FIRAnalytics.logEvent(
-                    withName:kFIREventSelectContent,
-                    parameters:parameters)
                 
             #endif
         }

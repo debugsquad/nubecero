2bffd4f8e551ef1c8821a83035f69fb8e0b53897
vauxhall <zero.griffin@gmail.com>
2016-10-28 18:42:39 -0500

    asset loader

--- nubecero/View/Main/VSpinner.swift
+++ nubecero/View/Main/VSpinner.swift
@@ -9,12 +9,12 @@ class VSpinner:UIImageView
         super.init(frame:CGRect.zero)
         
         let images:[UIImage] = [
-            #imageLiteral(resourceName: "assetLoader0"),
-            #imageLiteral(resourceName: "assetLoader1"),
-            #imageLiteral(resourceName: "assetLoader2"),
-            #imageLiteral(resourceName: "assetLoader3"),
-            #imageLiteral(resourceName: "assetLoader4"),
-            #imageLiteral(resourceName: "assetLoader5")
+            ,
+            ,
+            ,
+            ,
+            ,
+            
         ]
         
         isUserInteractionEnabled = false

14c52070845f5e6ee1559f470389e7cce0479edb
vauxhall <zero.griffin@gmail.com>
2016-10-28 17:51:34 -0500

    home catalog

--- nubecero/View/Home/VHomeCellUpload.swift
+++ nubecero/View/Home/VHomeCellUpload.swift
@@ -2,5 +2,40 @@ import UIKit
 
 class VHomeCellUpload:VHomeCell
 {
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.bold(size:12)
+        label.textColor = UIColor.complement
+        label.textAlignment = NSTextAlignment.center
+        label.text = NSLocalizedString("VHomeCellUpload_label", comment:"")
+        
+        addSubview(label)
+        
+        let views:[String:UIView] = [
+            "label":label]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
     
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }

bf400485518804410a061a58aed94b51b45fe1a9
vauxhall <zero.griffin@gmail.com>
2016-10-28 18:12:53 -0500

    home upload title

--- nubecero/Controller/Home/CHomeUpload.swift
+++ nubecero/Controller/Home/CHomeUpload.swift
@@ -2,5 +2,18 @@ import UIKit
 
 class CHomeUpload:CController
 {
+    weak var viewUpload:VHomeUpload!
     
+    override func loadView()
+    {
+        let viewUpload:VHomeUpload = VHomeUpload(controller:self)
+        self.viewUpload = viewUpload
+        view = viewUpload
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        title = NSLocalizedString("CHomeUpload_title", comment:"")
+    }
 }

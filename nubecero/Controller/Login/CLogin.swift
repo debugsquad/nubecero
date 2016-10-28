378b866e458df1db78110f27c0cd3edaa3684ae2
vauxhall <zero.griffin@gmail.com>
2016-10-28 13:45:25 -0500

    getting firebase credential

--- nubecero/Controller/Login/CLogin.swift
+++ nubecero/Controller/Login/CLogin.swift
@@ -1,5 +1,6 @@
 import UIKit
 import FBSDKLoginKit
+import FirebaseAuth
 
 class CLogin:CController, FBSDKLoginButtonDelegate
 {
@@ -12,15 +13,36 @@ class CLogin:CController, FBSDKLoginButtonDelegate
         view = viewLogin
     }
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        print("user id: \(FBSDKAccessToken.current()?.userID)")
+    }
+    
     //MARK: login button delegate
     
     func loginButton(_ loginButton:FBSDKLoginButton!, didCompleteWith result:FBSDKLoginManagerLoginResult!, error:Error!)
     {
-        
+        if let error:Error = error
+        {
+            print("facebook error")
+            print(error.localizedDescription)
+        }
+        else
+        {
+            let firebaseCredential:FIRAuthCredential = FIRFacebookAuthProvider.credential(
+                withAccessToken:FBSDKAccessToken.current().tokenString)
+            FIRAuth.auth()?.signIn(
+                with:firebaseCredential)
+            { [weak self] (user, error) in
+                
+                
+            }
+        }
     }
     
     func loginButtonDidLogOut(_ loginButton:FBSDKLoginButton!)
     {
-        print("facebook did log out")
     }
 }

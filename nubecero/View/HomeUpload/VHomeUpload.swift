5f18e6d1cd997ca7c62ffa286f66248e786dfc7d
vauxhall <zero.griffin@gmail.com>
2016-10-28 18:45:03 -0500

    view spinner

--- nubecero/View/HomeUpload/VHomeUpload.swift
+++ nubecero/View/HomeUpload/VHomeUpload.swift
@@ -3,6 +3,7 @@ import UIKit
 class VHomeUpload:UIView
 {
     weak var controller:CHomeUpload!
+    weak var spinner:VSpinner!
     
     convenience init(controller:CHomeUpload)
     {
@@ -11,5 +12,26 @@ class VHomeUpload:UIView
         translatesAutoresizingMaskIntoConstraints = false
         clipsToBounds = true
         self.controller = controller
+        
+        let spinner:VSpinner = VSpinner()
+        self.spinner = spinner
+        
+        addSubview(spinner)
+        
+        let views:[String:UIView] = [
+            "spinner":spinner]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[spinner]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[spinner]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }

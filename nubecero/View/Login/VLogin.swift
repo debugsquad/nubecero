575906186cf2bb5f2cf141cce8ddc3225249f3ba
vauxhall <zero.griffin@gmail.com>
2016-10-28 12:11:48 -0500

    added logo to login view

--- nubecero/View/Login/VLogin.swift
+++ nubecero/View/Login/VLogin.swift
@@ -11,5 +11,30 @@ class VLogin:UIView
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor.main
         self.controller = controller
+        
+        let logoView:UIImageView = UIImageView()
+        logoView.isUserInteractionEnabled = false
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+        logoView.clipsToBounds = true
+        logoView.contentMode = UIViewContentMode.center
+        logoView.image = #imageLiteral(resourceName: "assetGenericLogo")
+        
+        addSubview(logoView)
+        
+        let views:[String:UIView] = [
+            "logoView":logoView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[logoView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[logoView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }

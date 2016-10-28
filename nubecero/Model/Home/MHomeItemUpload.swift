9153f2099d7f0bb3c2ac064f978e7304a61c2783
vauxhall <zero.griffin@gmail.com>
2016-10-28 17:56:53 -0500

    upload pictures

--- nubecero/Model/Home/MHomeItemUpload.swift
+++ nubecero/Model/Home/MHomeItemUpload.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class MHomeItemUpload:MHomeItem
 {
-    private let kCellHeight:CGFloat = 100
+    private let kCellHeight:CGFloat = 70
     private let kSelectable:Bool = true
     
     init()
--- nubecero/View/Home/VHomeCellUpload.swift
+++ nubecero/View/Home/VHomeCellUpload.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class VHomeCellUpload:VHomeCell
 {
+    private let kImageHeight:CGFloat = 40
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
@@ -15,12 +17,22 @@ class VHomeCellUpload:VHomeCell
         label.textAlignment = NSTextAlignment.center
         label.text = NSLocalizedString("VHomeCellUpload_label", comment:"")
         
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "assetHomeUpload")
+        
         addSubview(label)
+        addSubview(imageView)
         
         let views:[String:UIView] = [
-            "label":label]
+            "label":label,
+            "imageView":imageView]
         
-        let metrics:[String:CGFloat] = [:]
+        let metrics:[String:CGFloat] = [
+            "imageHeight":kImageHeight]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[label]-0-|",
@@ -28,7 +40,12 @@ class VHomeCellUpload:VHomeCell
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[label]-0-|",
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-5-[imageView(imageHeight)]-(-5)-[label]-10-|",
             options:[],
             metrics:metrics,
             views:views))

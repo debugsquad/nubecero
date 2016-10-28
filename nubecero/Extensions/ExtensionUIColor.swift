7db72f87f8f77253bd3ce130cdb663926c06fe3f
vauxhall <zero.griffin@gmail.com>
2016-10-28 10:10:19 -0500

    edited colours

--- nubecero/Extensions/ExtensionUIColor.swift
+++ nubecero/Extensions/ExtensionUIColor.swift
@@ -2,11 +2,19 @@ import UIKit
 
 extension UIColor
 {
-    open class var main:UIColor //#4D80B3
+    open class var main:UIColor //#FF5B3A
     {
         get
         {
-            return UIColor(red:0.3, green:0.5, blue:0.7, alpha:1)
+            return UIColor(red:1, green:0.356862745098039, blue:0.227450980392157, alpha:1)
+        }
+    }
+    
+    open class var complement:UIColor //#0B99C4
+    {
+        get
+        {
+            return UIColor(red:0.043137254901961, green:0.6, blue:0.768627450980392, alpha:1)
         }
     }
     

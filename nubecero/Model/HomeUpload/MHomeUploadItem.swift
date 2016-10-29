a226110208c7d87a1035c0b1b5583425571ff74f
vauxhall <zero.griffin@gmail.com>
2016-10-29 15:55:21 -0500

    requesting image

--- nubecero/Model/HomeUpload/MHomeUploadItem.swift
+++ nubecero/Model/HomeUpload/MHomeUploadItem.swift
@@ -1,6 +1,41 @@
 import Foundation
+import Photos
 
 class MHomeUploadItem
 {
+    weak var asset:PHAsset!
+    var image:UIImage?
+    var requestId:PHImageRequestID?
+    let kImageSide:CGFloat = 150
+    let imageSize:CGSize
     
+    init(asset:PHAsset)
+    {
+        imageSize = CGSize(width:kImageSide, height:kImageSide)
+        self.asset = asset
+        
+        let requestOptions:PHImageRequestOptions = PHImageRequestOptions()
+        requestOptions.resizeMode = PHImageRequestOptionsResizeMode.fast
+        requestOptions.isSynchronous = false
+        requestOptions.deliveryMode = PHImageRequestOptionsDeliveryMode.fastFormat
+        
+        requestId = PHImageManager.default().requestImage(
+            for:asset,
+            targetSize:imageSize,
+            contentMode:PHImageContentMode.aspectFill,
+            options:requestOptions)
+        { [weak self] (image, info) in
+            
+            self?.requestId = nil
+            self?.image = image
+        }
+    }
+    
+    deinit
+    {
+        if let requestId:PHImageRequestID = requestId
+        {
+            PHImageManager.default().cancelImageRequest(requestId)
+        }
+    }
 }

5f231c73ab35128e3bcfdf6f31a82682244b8986
vauxhall <zero.griffin@gmail.com>
2016-10-29 12:29:45 -0500

    requesting access to photo library

--- nubecero/Controller/Home/CHomeUpload.swift
+++ nubecero/Controller/Home/CHomeUpload.swift
@@ -16,24 +16,51 @@ class CHomeUpload:CController
     {
         super.viewDidLoad()
         title = NSLocalizedString("CHomeUpload_title", comment:"")
-    }
-    
-    override func viewDidAppear(_ animated:Bool)
-    {
+        
         switch PHPhotoLibrary.authorizationStatus()
         {
             case PHAuthorizationStatus.denied,
-                 PHAuthorizationStatus.restricted,
-                 PHAuthorizationStatus.notDetermined:
-            
-                let errorMessage:String = NSLocalizedString("CHomeUpload_authDenied", comment:"")
-                VAlert.message(message:errorMessage)
+                 PHAuthorizationStatus.restricted:
+                
+                authDenied()
                 
                 break
-            
+                
+            case PHAuthorizationStatus.notDetermined:
+                
+                PHPhotoLibrary.requestAuthorization()
+                { [weak self] (status) in
+                    
+                    if status == PHAuthorizationStatus.authorized
+                    {
+                        self?.authorized()
+                    }
+                    else
+                    {
+                        self?.authDenied()
+                    }
+                }
+                
+                break
+                
             case PHAuthorizationStatus.authorized:
+                
+                authorized()
                 
                 break
         }
+    }
+    
+    //MARK: private
+    
+    private func authDenied()
+    {
+        let errorMessage:String = NSLocalizedString("CHomeUpload_authDenied", comment:"")
+        VAlert.message(message:errorMessage)
+    }
+    
+    private func authorized()
+    {
+        print("start loading pictures")
     }
 }

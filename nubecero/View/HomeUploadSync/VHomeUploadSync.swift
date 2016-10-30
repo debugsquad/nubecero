aeb50b8d8336a5c3a61895f21dcda1b9647117cb
vauxhall <zero.griffin@gmail.com>
2016-10-29 21:08:04 -0500

    viewBar

--- nubecero/View/HomeUploadSync/VHomeUploadSync.swift
+++ nubecero/View/HomeUploadSync/VHomeUploadSync.swift
@@ -2,7 +2,8 @@ import UIKit
 
 class VHomeUploadSync:UIView
 {
-    weak var controller:CHomeUploadSync!
+    weak var viewBar:VHomeUploadSyncBar!
+    private weak var controller:CHomeUploadSync!
     
     convenience init(controller:CHomeUploadSync)
     {
@@ -12,18 +13,25 @@ class VHomeUploadSync:UIView
         backgroundColor = UIColor.clear
         self.controller = controller
         
+        let barHeight:CGFloat = controller.parentController.viewParent.kBarHeight
         let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.extraLight)
         let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
         visualEffect.translatesAutoresizingMaskIntoConstraints = false
         visualEffect.clipsToBounds = true
         visualEffect.isUserInteractionEnabled = false
         
+        let viewBar:VHomeUploadSyncBar = VHomeUploadSyncBar(controller:controller)
+        self.viewBar = viewBar
+        
         addSubview(visualEffect)
+        addSubview(viewBar)
         
         let views:[String:UIView] = [
-            "visualEffect":visualEffect]
+            "visualEffect":visualEffect,
+            "viewBar":viewBar]
         
-        let metrics:[String:CGFloat] = [:]
+        let metrics:[String:CGFloat] = [
+            "barHeight":barHeight]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[visualEffect]-0-|",
@@ -32,6 +40,16 @@ class VHomeUploadSync:UIView
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"V:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[viewBar]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[viewBar(barHeight)]",
             options:[],
             metrics:metrics,
             views:views))

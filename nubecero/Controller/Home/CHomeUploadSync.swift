10dca986a599854321ba1be1bf09f5dbc2483708
vauxhall <zero.griffin@gmail.com>
2016-10-29 20:25:25 -0500

    view home upload sync

--- nubecero/Controller/Home/CHomeUploadSync.swift
+++ nubecero/Controller/Home/CHomeUploadSync.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CHomeUploadSync:CController
 {
+    weak var viewSync:VHomeUploadSync!
     
+    override func loadView()
+    {
+        let viewSync:VHomeUploadSync = VHomeUploadSync(controller:self)
+        self.viewSync = viewSync
+        view = viewSync
+    }
 }
---
+++ nubecero/View/HomeUploadSync/VHomeUploadSync.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+class VHomeUploadSync:UIView
+{
+    weak var controller:CHomeUploadSync!
+    
+    convenience init(controller:CHomeUploadSync)
+    {
+        self.init()
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = UIColor.clear
+        self.controller = controller
+        
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.light)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        visualEffect.isUserInteractionEnabled = false
+        
+        addSubview(visualEffect)
+        
+        let views:[String:UIView] = [
+            "visualEffect":visualEffect]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+}

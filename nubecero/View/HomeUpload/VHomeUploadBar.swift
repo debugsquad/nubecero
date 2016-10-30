302f94d2e205adbd7acba40510dca1f27c0bb8fb
vauxhall <zero.griffin@gmail.com>
2016-10-29 19:02:06 -0500

    view home upload bar

--- nubecero/View/HomeUpload/VHomeUploadBar.swift
+++ nubecero/View/HomeUpload/VHomeUploadBar.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class VHomeUploadBar:UIView
 {
+    weak var controller:CHomeUpload!
     
+    convenience init(controller:CHomeUpload)
+    {
+        self.init()
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }

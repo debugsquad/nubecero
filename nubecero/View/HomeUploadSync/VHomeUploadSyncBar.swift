4011667a46b743a2117b63a4bbeff26581fd15b0
vauxhall <zero.griffin@gmail.com>
2016-10-31 12:27:15 -0600

    updating label count

--- nubecero/View/HomeUploadSync/VHomeUploadSyncBar.swift
+++ nubecero/View/HomeUploadSync/VHomeUploadSyncBar.swift
@@ -4,10 +4,12 @@ class VHomeUploadSyncBar:UIView
 {
     private weak var controller:CHomeUploadSync!
     private weak var spinner:VSpinner!
+    private weak var labelCount:UILabel!
     private weak var layoutCancelButtonLeft:NSLayoutConstraint!
-    private let kSpinnerHeight:CGFloat = 60
-    private let kCancelButtonWith:CGFloat = 120
+    private let kSpinnerHeight:CGFloat = 80
+    private let kCancelButtonWith:CGFloat = 90
     private let kCancelButtonHeight:CGFloat = 34
+    private let kLabelCountWidth:CGFloat = 60
     private let kCornerRadius:CGFloat = 4
     
     convenience init(controller:CHomeUploadSync)
@@ -41,26 +43,34 @@ class VHomeUploadSyncBar:UIView
             action:#selector(actionCancel(sender:)),
             for:UIControlEvents.touchUpInside)
         
+        let labelCount:UILabel = UILabel()
+        labelCount.isUserInteractionEnabled = false
+        labelCount.translatesAutoresizingMaskIntoConstraints = false
+        labelCount.backgroundColor = UIColor.clear
+        labelCount.font = UIFont.medium(size:13)
+        labelCount.textColor = UIColor.black
+        labelCount.textAlignment = NSTextAlignment.right
+        self.labelCount = labelCount
+        
         addSubview(border)
         addSubview(cancelButton)
         addSubview(spinner)
+        addSubview(labelCount)
         
         let views:[String:UIView] = [
             "border":border,
-            "cancelButton":cancelButton]
+            "cancelButton":cancelButton,
+            "spinner":spinner,
+            "labelCount":labelCount]
         
         let metrics:[String:CGFloat] = [
             "spinnerHeight":kSpinnerHeight,
-            "cancelButtonWith":kCancelButtonWith,
-            "cancelButtonHeight":kCancelButtonHeight]
+            "cancelButtonWidth":kCancelButtonWith,
+            "cancelButtonHeight":kCancelButtonHeight,
+            "labelCountWidth":kLabelCountWidth]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[border]-0-|",
-            options:[],
-            metrics:metrics,
-            views:views))
-        addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:|-0-[cancelButton]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -70,12 +80,17 @@ class VHomeUploadSyncBar:UIView
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:[cancelButton(cancelButtonWidth)]",
+            withVisualFormat:"H:[cancelButton(cancelButtonWidth)]-0-[labelCount(labelCountWidth)]",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:[cancelButton(cancelButtonHeight)]-0-[spinner(spinnerHeight)]-0-[border(1)]-0-|",
+            withVisualFormat:"V:[cancelButton(cancelButtonHeight)]-10-[spinner(spinnerHeight)]-0-[border(1)]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[labelCount(cancelButtonHeight)]-10-[spinner]",
             options:[],
             metrics:metrics,
             views:views))
@@ -90,12 +105,14 @@ class VHomeUploadSyncBar:UIView
             constant:0)
         
         addConstraint(layoutCancelButtonLeft)
+        update()
     }
     
     override func layoutSubviews()
     {
         let width:CGFloat = bounds.maxX
-        let remainCancelButton:CGFloat = width - kCancelButtonWith
+        let cancelButtonLabelWidth:CGFloat = kCancelButtonWith + kLabelCountWidth
+        let remainCancelButton:CGFloat = width - cancelButtonLabelWidth
         let marginCancelButton:CGFloat = remainCancelButton / 2.0
         layoutCancelButtonLeft.constant = marginCancelButton
         
@@ -107,5 +124,42 @@ class VHomeUploadSyncBar:UIView
     func actionCancel(sender button:UIButton)
     {
         controller.cancelSync()
+    }
+    
+    //MARK: public
+    
+    func update()
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            guard
+                
+                let uploadItems:[MHomeUploadItem] = self?.controller.uploadItems
+            
+            else
+            {
+                return
+            }
+            
+            let totalItems:Int = uploadItems.count
+            var uploadedItems:Int = 0
+            
+            for item:MHomeUploadItem in uploadItems
+            {
+                if item.status == MHomeUploadItem.Status.Synced
+                {
+                    uploadedItems += 1
+                }
+            }
+            
+            let countText:String = "\(uploadedItems)/\(totalItems)"
+            
+            DispatchQueue.main.async
+            { [weak self] in
+                
+                self?.labelCount.text = countText
+            }
+        }
     }
 }

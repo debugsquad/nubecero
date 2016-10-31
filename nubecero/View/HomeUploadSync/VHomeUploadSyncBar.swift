7ab5900a397e6d28ee71258e176d97e8770705ee
vauxhall <zero.griffin@gmail.com>
2016-10-31 11:18:45 -0600

    cancel button constraints

--- nubecero/View/HomeUploadSync/VHomeUploadSyncBar.swift
+++ nubecero/View/HomeUploadSync/VHomeUploadSyncBar.swift
@@ -3,6 +3,12 @@ import UIKit
 class VHomeUploadSyncBar:UIView
 {
     private weak var controller:CHomeUploadSync!
+    private weak var spinner:VSpinner!
+    private weak var layoutCancelButtonLeft:NSLayoutConstraint!
+    private let kSpinnerHeight:CGFloat = 60
+    private let kCancelButtonWith:CGFloat = 120
+    private let kCancelButtonHeight:CGFloat = 34
+    private let kCornerRadius:CGFloat = 4
     
     convenience init(controller:CHomeUploadSync)
     {
@@ -12,17 +18,24 @@ class VHomeUploadSyncBar:UIView
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
         
+        let spinner:VSpinner = VSpinner()
+        self.spinner = spinner
+        
         let border:UIView = UIView()
         border.backgroundColor = UIColor.black
         border.isUserInteractionEnabled = false
         border.translatesAutoresizingMaskIntoConstraints = false
         
         let cancelButton:UIButton = UIButton()
+        cancelButton.backgroundColor = UIColor.main
         cancelButton.translatesAutoresizingMaskIntoConstraints = false
-        cancelButton.setTitleColor(UIColor.black, for:UIControlState.normal)
-        cancelButton.setTitleColor(UIColor.white, for:UIControlState.highlighted)
-        cancelButton.setTitle(NSLocalizedString("VHomeUploadSyncBar_cancel", comment:""), for:UIControlState.normal)
-        cancelButton.titleLabel!.font = UIFont.medium(size:14)
+        cancelButton.setTitleColor(UIColor.white, for:UIControlState.normal)
+        cancelButton.setTitleColor(UIColor.black, for:UIControlState.highlighted)
+        cancelButton.setTitle(
+            NSLocalizedString("VHomeUploadSyncBar_cancel", comment:""),
+            for:UIControlState.normal)
+        cancelButton.titleLabel!.font = UIFont.medium(size:13)
+        cancelButton.layer.cornerRadius = kCornerRadius
         cancelButton.addTarget(
             self,
             action:#selector(actionCancel(sender:)),
@@ -30,20 +43,19 @@ class VHomeUploadSyncBar:UIView
         
         addSubview(border)
         addSubview(cancelButton)
+        addSubview(spinner)
         
         let views:[String:UIView] = [
             "border":border,
             "cancelButton":cancelButton]
         
-        let metrics:[String:CGFloat] = [:]
+        let metrics:[String:CGFloat] = [
+            "spinnerHeight":kSpinnerHeight,
+            "cancelButtonWith":kCancelButtonWith,
+            "cancelButtonHeight":kCancelButtonHeight]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[border]-0-|",
-            options:[],
-            metrics:metrics,
-            views:views))
-        addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:[border(1)]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -53,10 +65,41 @@ class VHomeUploadSyncBar:UIView
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-20-[cancelButton]-0-|",
+            withVisualFormat:"H:|-0-[spinner]-0-|",
             options:[],
             metrics:metrics,
             views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:[cancelButton(cancelButtonWidth)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[cancelButton(cancelButtonHeight)]-0-[spinner(spinnerHeight)]-0-[border(1)]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        
+        layoutCancelButtonLeft = NSLayoutConstraint(
+            item:cancelButton,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:0)
+        
+        addConstraint(layoutCancelButtonLeft)
+    }
+    
+    override func layoutSubviews()
+    {
+        let width:CGFloat = bounds.maxX
+        let remainCancelButton:CGFloat = width - kCancelButtonWith
+        let marginCancelButton:CGFloat = remainCancelButton / 2.0
+        layoutCancelButtonLeft.constant = marginCancelButton
+        
+        super.layoutSubviews()
     }
     
     //MARK: actions

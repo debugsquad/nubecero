e997ac9ea51e90508af8870abf75a6c9e88beeb0
vauxhall <zero.griffin@gmail.com>
2016-11-01 14:20:57 -0600

    cell size weight

--- nubecero/Model/PicturesDetail/MPicturesDetailItem.swift
+++ nubecero/Model/PicturesDetail/MPicturesDetailItem.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class MPicturesDetailItem
 {
+    let reusableIdentifier:String
+    let sizeWeight:Int
     
+    init(reusableIdentifier:String, sizeWeight:Int)
+    {
+        self.reusableIdentifier = reusableIdentifier
+        self.sizeWeight = sizeWeight
+    }
 }

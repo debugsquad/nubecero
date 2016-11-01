b56d33b9020a7e431fbb09dccb85e744f1c4f167
vauxhall <zero.griffin@gmail.com>
2016-11-01 10:49:17 -0600

    controller and view pictures

--- nubecero/Controller/Pictures/CPictures.swift
+++ nubecero/Controller/Pictures/CPictures.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CPictures:CController
 {
+    weak var viewPictures:VPictures!
     
+    override func loadView()
+    {
+        let viewPictures:VPictures = VPictures(controller:self)
+        self.viewPictures = viewPictures
+        view = viewPictures
+    }
 }

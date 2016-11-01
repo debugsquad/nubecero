340879924080a0d3edad14a0e0ce09c42ae3cf36
vauxhall <zero.griffin@gmail.com>
2016-11-01 15:05:06 -0600

    added image view to cell

--- nubecero/View/Pictures/VPicturesCell.swift
+++ nubecero/View/Pictures/VPicturesCell.swift
@@ -2,10 +2,37 @@ import UIKit
 
 class VPicturesCell:UICollectionViewCell
 {
+    weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "imageView":imageView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)

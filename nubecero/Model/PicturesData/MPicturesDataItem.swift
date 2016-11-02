a269dd9432845cc8b13579b85b5c89f629368379
vauxhall <zero.griffin@gmail.com>
2016-11-02 14:37:05 -0600

    item close

--- nubecero/Model/PicturesData/MPicturesDataItem.swift
+++ nubecero/Model/PicturesData/MPicturesDataItem.swift
@@ -1,6 +1,18 @@
-import Foundation
+import UIKit
 
 class MPicturesDataItem
 {
+    let reusableIdentifier:String
+    let cellHeight:CGFloat
     
+    init()
+    {
+        fatalError()
+    }
+    
+    init(reusableIdentifier:String, cellHeight:CGFloat)
+    {
+        self.reusableIdentifier = reusableIdentifier
+        self.cellHeight = cellHeight
+    }
 }
--- nubecero/Model/PicturesData/MPicturesDataItemClose.swift
+++ nubecero/Model/PicturesData/MPicturesDataItemClose.swift
@@ -1,6 +1,14 @@
-import Foundation
+import UIKit
 
 class MPicturesDataItemClose:MPicturesDataItem
 {
+    private let kCellHeight:CGFloat = 70
     
+    override init()
+    {
+        let reusableIdentifier:String = VPicturesDataCellClose.reusableIdentifier
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            cellHeight:kCellHeight)
+    }
 }

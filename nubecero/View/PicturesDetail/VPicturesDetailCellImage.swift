124f8ef05a0c257c9959424c44bcd6de3f801283
vauxhall <zero.griffin@gmail.com>
2016-11-02 09:11:26 -0600

    added imageView to detail cell

--- nubecero/View/PicturesDetail/VPicturesDetailCellImage.swift
+++ nubecero/View/PicturesDetail/VPicturesDetailCellImage.swift
@@ -2,10 +2,37 @@ import UIKit
 
 class VPicturesDetailCellImage:VPicturesDetailCell
 {
+    weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "imageView":imageView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)

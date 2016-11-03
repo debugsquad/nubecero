08350ce32dd1ef71d1b4ed9ce6deb89f15813217
vauxhall <zero.griffin@gmail.com>
2016-11-03 09:36:28 -0600

    View home cell disk

---
+++ nubecero/View/Home/VHomeCellDisk.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+class VHomeCellDisk:VHomeCell
+{
+    private let kImageHeight:CGFloat = 40
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.medium(size:12)
+        label.textColor = UIColor.complement
+        label.textAlignment = NSTextAlignment.center
+        label.text = NSLocalizedString("VHomeCellUpload_label", comment:"")
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = #imageLiteral(resourceName: "assetHomeUpload")
+        
+        addSubview(label)
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "label":label,
+            "imageView":imageView]
+        
+        let metrics:[String:CGFloat] = [
+            "imageHeight":kImageHeight]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-5-[imageView(imageHeight)]-(-5)-[label]-10-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}

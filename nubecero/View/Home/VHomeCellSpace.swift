4a82c3299d40b39cf85e15083a61ab15febcd36d
vauxhall <zero.griffin@gmail.com>
2016-11-03 17:46:01 -0600

    view home cell space

---
+++ nubecero/View/Home/VHomeCellSpace.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class VHomeCellSpace:VHomeCell
+{
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func config(controller:CHome, model:MHomeItem)
+    {
+        
+    }
+}

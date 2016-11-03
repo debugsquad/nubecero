f60180bf291aae7fd700f299154c7b81d15d4578
vauxhall <zero.griffin@gmail.com>
2016-11-03 09:42:28 -0600

    model item disk

---
+++ nubecero/Model/Home/MHomeItemDisk.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class MHomeItemDisk:MHomeItem
+{
+    private let kCellHeight:CGFloat = 250
+    private let kSelectable:Bool = false
+    
+    override init()
+    {
+        let reusableIdentifier:String = VHomeCellDisk.reusableIdentifier
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            cellHeight:kCellHeight,
+            selectable:kSelectable)
+    }
+}

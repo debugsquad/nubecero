c4d4a00eadfec0834a53603cab9bf4dcf6e4cb93
vauxhall <zero.griffin@gmail.com>
2016-11-03 15:57:18 -0600

    firebase model status

--- nubecero/Firebase/Database/Model/FDatabaseModelUserStatus.swift
+++ nubecero/Firebase/Database/Model/FDatabaseModelUserStatus.swift
@@ -2,71 +2,27 @@ import Foundation
 
 class FDatabaseModelUserStatus:FDatabaseModel
 {
-    enum Property:String
-    {
-        case status = "status"
-        case created = "created"
-        case lastSession = "lastSession"
-        case diskUsed = "diskUsed"
-        case pictures = "pictures"
-    }
-    
-    let created:TimeInterval
-    let lastSession:TimeInterval
-    let diskUsed:Int
-    private let kNoTime:TimeInterval = 0
-    
-    override init()
-    {
-        created = NSDate().timeIntervalSince1970
-        lastSession = created
-        diskUsed = 0
-        
-        super.init()
-    }
+    let status:FDatabaseModelUser.Status
     
     required init(snapshot:Any)
     {
-        let snapshotDict:[String:Any]? = snapshot as? [String:Any]
-        
-        if let created:TimeInterval = snapshotDict?[Property.created.rawValue] as? TimeInterval
+        if let statusInt:Int = snapshot as? Int
         {
-            self.created = created
+            if let status:FDatabaseModelUser.Status = FDatabaseModelUser.Status(
+                rawValue:statusInt)
+            {
+                self.status = status
+            }
+            else
+            {
+                self.status = FDatabaseModelUser.Status.unknown
+            }
         }
         else
         {
-            self.created = kNoTime
-        }
-        
-        if let lastSession:TimeInterval = snapshotDict?[Property.lastSession.rawValue] as? TimeInterval
-        {
-            self.lastSession = lastSession
-        }
-        else
-        {
-            self.lastSession = kNoTime
-        }
-        
-        if let diskUsed:Int = snapshotDict?[Property.diskUsed.rawValue] as? Int
-        {
-            self.diskUsed = diskUsed
-        }
-        else
-        {
-            self.diskUsed = 0
+            self.status = FDatabaseModelUser.Status.unknown
         }
         
         super.init()
-    }
-    
-    override func modelJson() -> Any
-    {
-        let json:[String:Any] = [
-            Property.created.rawValue:created,
-            Property.lastSession.rawValue:lastSession,
-            Property.diskUsed.rawValue:diskUsed
-        ]
-        
-        return json
     }
 }

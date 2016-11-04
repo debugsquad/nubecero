1866d6fee150766549175fed34442c4356ba5105
vauxhall <zero.griffin@gmail.com>
2016-11-04 12:01:01 -0600

    added item security

--- nubecero/Model/Settings/MSettings.swift
+++ nubecero/Model/Settings/MSettings.swift
@@ -6,7 +6,10 @@ class MSettings
     
     init()
     {
+        let itemSecurity:MSettingsItemSecurity = MSettingsItemSecurity()
+        
         items = [
+            itemSecurity
         ]
     }
 }

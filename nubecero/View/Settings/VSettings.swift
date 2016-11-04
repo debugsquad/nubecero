762dfa17ab5b9ef616a90606bef864d703d49f01
vauxhall <zero.griffin@gmail.com>
2016-11-04 12:11:11 -0600

    constraints for collection

--- nubecero/View/Settings/VSettings.swift
+++ nubecero/View/Settings/VSettings.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-class VSettings:UIView
+class VSettings:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CSettings!
+    private weak var collectionView:UICollectionView!
+    private let kCollectionBottom:CGFloat = 30
+    private let kInterLine:CGFloat = 1
     
     convenience init(controller:CSettings)
     {
@@ -11,5 +14,51 @@ class VSettings:UIView
         backgroundColor = UIColor.background
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let barHeight:CGFloat = controller.parentController.viewParent.kBarHeight
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = kInterLine
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        flow.sectionInset = UIEdgeInsets(top:0, left:0, bottom:kCollectionBottom, right:0)
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VSettingsCellSecurity.self,
+            forCellWithReuseIdentifier:
+            VSettingsCellSecurity.reusableIdentifier)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        let views:[String:UIView] = [
+            "collectionView":collectionView]
+        
+        let metrics:[String:CGFloat] = [
+            "barHeight":barHeight]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-(barHeight)-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }

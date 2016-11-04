c96d56c7e24222b3f942d94786616c6b32176221
vauxhall <zero.griffin@gmail.com>
2016-11-04 13:51:05 -0600

    cell logout

--- nubecero/View/Settings/VSettingsCellLogout.swift
+++ nubecero/View/Settings/VSettingsCellLogout.swift
@@ -2,50 +2,28 @@ import UIKit
 
 class VSettingsCellLogout:VSettingsCell
 {
-    private weak var check:UISwitch!
-    private let kCheckWidth:CGFloat = 72
-    private let kCheckTop:CGFloat = 23
-    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
-        
-        let check:UISwitch = UISwitch()
-        check.translatesAutoresizingMaskIntoConstraints = false
-        check.onTintColor = UIColor.main
-        check.addTarget(
-            self,
-            action:#selector(actionCheck(sender:)),
-            for:UIControlEvents.valueChanged)
-        self.check = check
         
         let label:UILabel = UILabel()
         label.isUserInteractionEnabled = false
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = UIColor.clear
-        label.font = UIFont.medium(size:15)
-        label.textColor = UIColor(white:0.5, alpha:1)
-        label.numberOfLines = 0
-        label.text = NSLocalizedString("VSettingsCellSecurity_label", comment:"")
+        label.font = UIFont.bold(size:16)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.center
+        label.text = NSLocalizedString("VSettingsCellLogout_label", comment:"")
         
         addSubview(label)
-        addSubview(check)
         
         let views:[String:UIView] = [
-            "check":check,
             "label":label]
         
-        let metrics:[String:CGFloat] = [
-            "checkWidth":kCheckWidth,
-            "checkTop":kCheckTop]
+        let metrics:[String:CGFloat] = [:]
         
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:|-10-[label]-20-[check(checkWidth)]-0-|",
-            options:[],
-            metrics:metrics,
-            views:views))
-        addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-(checkTop)-[check]",
+            withVisualFormat:"H:|-0-[label]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -59,29 +37,5 @@ class VSettingsCellLogout:VSettingsCell
     required init?(coder:NSCoder)
     {
         fatalError()
-    }
-    
-    override func config(model:MSettingsItem)
-    {
-        guard
-            
-            let securityOn:Bool = MSession.sharedInstance.settings?.security
-            
-            else
-        {
-            return
-        }
-        
-        check.isOn = securityOn
-    }
-    
-    //MARK: actions
-    
-    func actionCheck(sender check:UISwitch)
-    {
-        let securityOn:Bool = check.isOn
-        MSession.sharedInstance.settings?.security = securityOn
-        
-        DManager.sharedInstance.save()
     }
 }

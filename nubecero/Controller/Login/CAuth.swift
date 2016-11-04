fda3bd9c93a45f5aa3c333aac6a17cf3036b02dc
vauxhall <zero.griffin@gmail.com>
2016-11-04 10:06:09 -0600

    controller auth uses view auth

--- nubecero/Controller/Login/CAuth.swift
+++ nubecero/Controller/Login/CAuth.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CAuth:CController
 {
+    weak var viewAuth:VAuth!
     
+    override func loadView()
+    {
+        let viewAuth:VAuth = VAuth(controller:self)
+        self.viewAuth = viewAuth
+        view = viewAuth
+    }
 }

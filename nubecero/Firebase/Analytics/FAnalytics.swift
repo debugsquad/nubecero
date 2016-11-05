f2f2d81e69300f348e4dab32b76efd7af7fd8079
vauxhall <zero.griffin@gmail.com>
2016-11-05 16:11:06 -0600

    added event upload pictures and logout

--- nubecero/Firebase/Analytics/FAnalytics.swift
+++ nubecero/Firebase/Analytics/FAnalytics.swift
@@ -5,6 +5,7 @@ class FAnalytics
 {
     private let kEventScreen:NSString = "Screen"
     private let kEventAction:NSString = "Action"
+    private let kEventActionLogout:NSString = "Logout"
     
     //MARK: public
     
@@ -15,6 +16,36 @@ class FAnalytics
             let parameters:[String:NSObject] = [
                 kFIRParameterContentType:self.kEventScreen,
                 kFIRParameterItemID:controller.name
+            ]
+            
+            FIRAnalytics.logEvent(
+                withName:kFIREventSelectContent,
+                parameters:parameters)
+        }
+    }
+    
+    func upload(pictures:Int)
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        {
+            let parameters:[String:NSObject] = [
+                kFIRParameterContentType:self.kEventAction,
+                kFIRParameterItemID:pictures as NSObject
+            ]
+            
+            FIRAnalytics.logEvent(
+                withName:kFIREventSelectContent,
+                parameters:parameters)
+        }
+    }
+    
+    func logout(pictures:Int)
+    {
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        {
+            let parameters:[String:NSObject] = [
+                kFIRParameterContentType:self.kEventAction,
+                kFIRParameterItemID:self.kEventActionLogout
             ]
             
             FIRAnalytics.logEvent(

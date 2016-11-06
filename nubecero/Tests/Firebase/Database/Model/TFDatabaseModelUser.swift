c9ac755160b310447d0776065f73ac174bc0bd2a
vauxhall <zero.griffin@gmail.com>
2016-11-05 18:47:20 -0600

    test init with snapshot

--- nubecero/Tests/Firebase/Database/Model/TFDatabaseModelUser.swift
+++ nubecero/Tests/Firebase/Database/Model/TFDatabaseModelUser.swift
@@ -1,9 +1,44 @@
 import XCTest
+@testable import nubecero
 
 class TFDatabaseModelUser:XCTestCase
 {
-    func testInit()
+    private let kCreated:TimeInterval = 123456
+    private let kLastSession:TimeInterval = 45678
+    private let kDiskUsed:Int = 3098765
+    
+    func testInitSnapshot()
     {
+        let status:FDatabaseModelUser.Status = FDatabaseModelUser.Status.active
         
+        let snapshot:[String:Any] = [
+            FDatabaseModelUser.Property.status.rawValue:status.rawValue,
+            FDatabaseModelUser.Property.created.rawValue:kCreated,
+            FDatabaseModelUser.Property.lastSession.rawValue:kLastSession,
+            FDatabaseModelUser.Property.diskUsed.rawValue:kDiskUsed
+        ]
+        
+        let fDatabaseModelUser:FDatabaseModelUser = FDatabaseModelUser(
+            snapshot:snapshot)
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.status,
+            status,
+            "Parsing status error")
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.created,
+            kCreated,
+            "Parsing created error")
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.lastSession,
+            kLastSession,
+            "Parsing last session error")
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.diskUsed,
+            kDiskUsed,
+            "Parsing disk used error")
     }
 }

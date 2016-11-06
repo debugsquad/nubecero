05e9e90a17fbda66211393f5efb477b6b1e6dbbe
vauxhall <zero.griffin@gmail.com>
2016-11-05 18:34:18 -0600

    testing image view

--- nubecero/Tests/View/Settings/TVSettingsCellProfile.swift
+++ nubecero/Tests/View/Settings/TVSettingsCellProfile.swift
@@ -3,12 +3,26 @@ import XCTest
 
 class TVSettingsCellProfile:XCTestCase
 {
+    private var vSettingsCellProfile:VSettingsCellProfile?
+    
+    override func setUp()
+    {
+        super.setUp()
+        vSettingsCellProfile = VSettingsCellProfile(frame:CGRect.zero)
+    }
+    
     func testUserName()
     {
-        let vSettingsCellProfile:VSettingsCellProfile = VSettingsCellProfile(frame:CGRect.zero)
-        let userName:String? = vSettingsCellProfile.userName.text
+        let userName:String? = vSettingsCellProfile?.userName.text
         
         XCTAssertNotNil(userName, "Username can't be nil")
         XCTAssertGreaterThan(userName!.characters.count, 0, "Username can't be empty")
+    }
+    
+    func testImageView()
+    {
+        let image:UIImage? = vSettingsCellProfile?.imageView.image
+        
+        XCTAssertNotNil(image, "Profile image can't be nil")
     }
 }

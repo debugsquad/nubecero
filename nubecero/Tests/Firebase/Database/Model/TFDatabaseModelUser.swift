fed8b03d7f8e8ff14942e78c9d3536e7fcc3deb6
vauxhall <zero.griffin@gmail.com>
2016-11-05 18:54:17 -0600

    creating tests for parsing firebase model user

--- nubecero/Tests/Firebase/Database/Model/TFDatabaseModelUser.swift
+++ nubecero/Tests/Firebase/Database/Model/TFDatabaseModelUser.swift
@@ -40,5 +40,61 @@ class TFDatabaseModelUser:XCTestCase
             fDatabaseModelUser.diskUsed,
             kDiskUsed,
             "Parsing disk used error")
+        
+        let modelJson:[String:Any]? = fDatabaseModelUser.modelJson() as? [String:Any]
+        
+        XCTAssertNotNil(
+            modelJson,
+            "Error creating model json")
+        
+        XCTAssertEqual(
+            modelJson![FDatabaseModelUser.Property.status.rawValue],
+            status.rawValue,
+            "Parsing json status error")
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.created,
+            kCreated,
+            "Parsing created error")
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.lastSession,
+            kLastSession,
+            "Parsing last session error")
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.diskUsed,
+            kDiskUsed,
+            "Parsing disk used error")
+    }
+    
+    func testInitSnapshotUserBanned()
+    {
+        let status:FDatabaseModelUser.Status = FDatabaseModelUser.Status.banned
+        
+        let snapshot:[String:Any] = [
+            FDatabaseModelUser.Property.status.rawValue:status.rawValue
+        ]
+        
+        let fDatabaseModelUser:FDatabaseModelUser = FDatabaseModelUser(
+            snapshot:snapshot)
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.status,
+            status,
+            "User banned not working")
+    }
+    
+    func testInitSnapshotNil()
+    {
+        let snapshot:[String:Any] = [:]
+        
+        let fDatabaseModelUser:FDatabaseModelUser = FDatabaseModelUser(
+            snapshot:snapshot)
+        
+        XCTAssertEqual(
+            fDatabaseModelUser.status,
+            FDatabaseModelUser.Status.unknown,
+            "Snapshot nil not using unknown status")
     }
 }

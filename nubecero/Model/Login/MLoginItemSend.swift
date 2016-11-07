6a7be7e077d3fac99c3eeff1e2835b5dffd09c72
vauxhall <zero.griffin@gmail.com>
2016-11-07 16:13:31 -0600

    hover for login cells

--- nubecero/Model/Login/MLoginItemSend.swift
+++ nubecero/Model/Login/MLoginItemSend.swift
@@ -2,7 +2,7 @@ import UIKit
 
 class MLoginItemSend:MLoginItem
 {
-    private let kCellHeight:CGFloat = 60
+    private let kCellHeight:CGFloat = 40
     private let kSelectable:Bool = true
     
     override init()

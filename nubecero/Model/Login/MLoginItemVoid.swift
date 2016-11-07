278c6303e76b3d3d0fa3adf05dd62806bf6c4a82
vauxhall <zero.griffin@gmail.com>
2016-11-07 16:17:15 -0600

    model item void

--- nubecero/Model/Login/MLoginItemVoid.swift
+++ nubecero/Model/Login/MLoginItemVoid.swift
@@ -1,13 +1,13 @@
 import UIKit
 
-class MLoginItemDisclaimerSignin:MLoginItem
+class MLoginItemVoid:MLoginItem
 {
-    private let kCellHeight:CGFloat = 50
+    private let kCellHeight:CGFloat = 30
     private let kSelectable:Bool = false
     
     override init()
     {
-        let reusableIdentifier:String = VLoginCellDisclaimerSignin.reusableIdentifier
+        let reusableIdentifier:String = VLoginCellVoid.reusableIdentifier
         super.init(
             reusableIdentifier:reusableIdentifier,
             cellHeight:kCellHeight,

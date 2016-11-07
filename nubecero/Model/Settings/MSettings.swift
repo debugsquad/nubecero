eeb73d0b5af49e00708fd77734b32db08369f1b6
vauxhall <zero.griffin@gmail.com>
2016-11-07 14:12:51 -0600

    removed Settings cells for Facebook

--- nubecero/Model/Settings/MSettings.swift
+++ nubecero/Model/Settings/MSettings.swift
@@ -6,17 +6,11 @@ class MSettings
     
     init()
     {
-        let itemProfile:MSettingsItemProfile = MSettingsItemProfile()
         let itemSecurity:MSettingsItemSecurity = MSettingsItemSecurity()
-        let itemFacebookShare:MSettingsItemFacebookShare = MSettingsItemFacebookShare()
-        let itemFacebookInvite:MSettingsItemFacebookInvite = MSettingsItemFacebookInvite()
         let itemLogout:MSettingsItemLogout = MSettingsItemLogout()
         
         items = [
-            itemProfile,
             itemSecurity,
-            itemFacebookShare,
-            itemFacebookInvite,
             itemLogout
         ]
     }

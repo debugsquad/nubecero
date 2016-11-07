e5d8a1f75ec87ec0cf082cb90f52b64b486032af
vauxhall <zero.griffin@gmail.com>
2016-11-06 22:01:41 -0600

    edited info.plist for facebook

--- nubecero/View/Settings/VSettingsCellFacebookShare.swift
+++ nubecero/View/Settings/VSettingsCellFacebookShare.swift
@@ -1,13 +1,31 @@
 import UIKit
-
+import FBSDKShareKit
 
 class VSettingsCellFacebookShare:VSettingsCell
 {
+    private let kStringUrl:String = "https://www.facebook.com/FacebookDevelopers"
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         
+        guard
+            
+            let url:URL = URL(string:kStringUrl)
         
+        else
+        {
+            return
+        }
+        
+        let fbContent:FBSDKShareLinkContent = FBSDKShareLinkContent()
+        fbContent.contentURL = url
+        
+        let fbButton:FBSDKShareButton = FBSDKShareButton()
+        fbButton.shareContent = fbContent
+        fbButton.center = center
+        
+        addSubview(fbButton)
     }
     
     required init?(coder:NSCoder)

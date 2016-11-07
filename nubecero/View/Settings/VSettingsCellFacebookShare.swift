1ab6b8064c8d8cd31b9be70c26726db210781994
vauxhall <zero.griffin@gmail.com>
2016-11-06 21:48:22 -0600

    view cell facebookShare

---
+++ nubecero/View/Settings/VSettingsCellFacebookShare.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class VSettingsCellFacebookShare:VSettingsCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}

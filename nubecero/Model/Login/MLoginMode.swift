9c35649a7fbad91aca0acd9e9ce93785e559c3f1
vauxhall <zero.griffin@gmail.com>
2016-11-07 15:30:11 -0600

    factory mode names

--- nubecero/Model/Login/MLoginMode.swift
+++ nubecero/Model/Login/MLoginMode.swift
@@ -4,6 +4,19 @@ class MLoginMode
 {
     let name:String
     
+    class func ModeNames() -> [String]
+    {
+        let modeRegister:MLoginModeRegister = MLoginModeRegister()
+        let modeSignup:MLoginModeSignup = MLoginModeSignup()
+        
+        let names:[String] = [
+            modeRegister.name,
+            modeSignup.name
+        ]
+        
+        return names
+    }
+    
     init()
     {
         fatalError()

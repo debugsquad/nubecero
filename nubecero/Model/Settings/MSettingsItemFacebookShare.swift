38a00c1d606dc6a152a39de5c81530ac43588cca
vauxhall <zero.griffin@gmail.com>
2016-11-06 21:46:27 -0600

    model item facebookShare

---
+++ nubecero/Model/Settings/MSettingsItemFacebookShare.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class MSettingsItemFacebookShare:MSettingsItem
+{
+    private let kCellHeight:CGFloat = 80
+    private let kSelectable:Bool = false
+    
+    override init()
+    {
+        let reusableIdentifier:String = VSettingsCellSecurity.reusableIdentifier
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            cellHeight:kCellHeight,
+            selectable:kSelectable)
+    }
+}

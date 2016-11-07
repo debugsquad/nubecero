079a8f6524fe205e8354b3907f5512fe376934bd
vauxhall <zero.griffin@gmail.com>
2016-11-07 14:31:33 -0600

    init for login item

--- nubecero/Model/Login/MLoginItem.swift
+++ nubecero/Model/Login/MLoginItem.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class MLoginItem
 {
+    let reusableIdentifier:String
+    let cellHeight:CGFloat
+    let selectable:Bool
     
+    init(reusableIdentifier:String, cellHeight:CGFloat, selectable:Bool)
+    {
+        self.reusableIdentifier = reusableIdentifier
+        self.cellHeight = cellHeight
+        self.selectable = selectable
+    }
 }

88308727535c2ac371622b304de064e4f73f1552
vauxhall <zero.griffin@gmail.com>
2016-11-07 18:44:22 -0600

    view settings cell about

---
+++ nubecero/View/Settings/VSettingsCellAbout.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class VSettingsCellAbout:VSettingsCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.bold(size:14)
+        label.textColor = UIColor.black
+        label.textAlignment = NSTextAlignment.center
+        label.text = NSLocalizedString("VSettingsCellLogout_label", comment:"")
+        
+        addSubview(label)
+        
+        let views:[String:UIView] = [
+            "label":label]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}

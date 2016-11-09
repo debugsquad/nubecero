71bb7c3e810af01ca9fb7a1cc0f7e45cdef3b30b
vauxhall <zero.griffin@gmail.com>
2016-11-09 14:23:12 -0600

    create credentials

--- nubecero/Model/OnboardForm/MOnboardForm.swift
+++ nubecero/Model/OnboardForm/MOnboardForm.swift
@@ -5,6 +5,10 @@ class MOnboardForm
     let items:[MOnboardFormItem]
     let title:String
     let buttonMessage:String
+    weak var itemEmail:MOnboardFormItemEmail!
+    weak var itemPassword:MOnboardFormItemPassword!
+    private let kEmailMinLength:Int = 4
+    private let kPasswordMinLength:Int = 6
 
     class func Register() -> MOnboardForm
     {
@@ -22,7 +26,9 @@ class MOnboardForm
         let model:MOnboardForm = MOnboardForm(
             items:items,
             title:title,
-            buttonMessage:buttonMessage)
+            buttonMessage:buttonMessage,
+            itemEmail:itemEmail,
+            itemPassword:itemPassword)
         
         return model
     }
@@ -43,7 +49,9 @@ class MOnboardForm
         let model:MOnboardForm = MOnboardForm(
             items:items,
             title:title,
-            buttonMessage:buttonMessage)
+            buttonMessage:buttonMessage,
+            itemEmail:itemEmail,
+            itemPassword:itemPassword)
         
         return model
     }
@@ -53,10 +61,80 @@ class MOnboardForm
         fatalError()
     }
     
-    private init(items:[MOnboardFormItem], title:String, buttonMessage:String)
+    //MARK: private
+    
+    private init(items:[MOnboardFormItem], title:String, buttonMessage:String, itemEmail:MOnboardFormItemEmail, itemPassword:MOnboardFormItemPassword)
     {
         self.items = items
         self.title = title
         self.buttonMessage = buttonMessage
+        self.itemEmail = itemEmail
+        self.itemPassword = itemPassword
+    }
+    
+    //MARK: public
+    
+    func createCredentials(completion:@escaping((MOnboardFormCredentials?, String?) -> ()))
+    {
+        let emailMinLength:Int = kEmailMinLength
+        let passwordMinLength:Int = kPasswordMinLength
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+        { [weak self] in
+            
+            let credentials:MOnboardFormCredentials?
+            let errorString:String?
+            
+            guard
+            
+                let email:String = self?.itemEmail.email
+            
+            else
+            {
+                credentials = nil
+                errorString = NSLocalizedString("MOnboardForm_errorEmail", comment:"")
+                completion(credentials, errorString)
+                
+                return
+            }
+            
+            if email.characters.count < emailMinLength
+            {
+                credentials = nil
+                errorString = NSLocalizedString("MOnboardForm_errorEmailShort", comment:"")
+                completion(credentials, errorString)
+                
+                return
+            }
+            
+            guard
+            
+                let password:String = self?.itemPassword.password
+            
+            else
+            {
+                credentials = nil
+                errorString = NSLocalizedString("MOnboardForm_errorPassword", comment:"")
+                completion(credentials, errorString)
+                
+                return
+            }
+            
+            if password.characters.count < passwordMinLength
+            {
+                credentials = nil
+                errorString = NSLocalizedString("MOnboardForm_errorPasswordShort", comment:"")
+                completion(credentials, errorString)
+                
+                return
+            }
+            
+            credentials = MOnboardFormCredentials(
+                email:email,
+                password:password)
+            errorString = nil
+            
+            completion(credentials, errorString)
+        }
     }
 }

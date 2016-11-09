ff1d326cef7a4ef370543a3e0d060ae8a70d50f6
vauxhall <zero.griffin@gmail.com>
2016-11-09 10:28:29 -0600

    added collection view

--- nubecero/View/OnboardForm/VOnboardForm.swift
+++ nubecero/View/OnboardForm/VOnboardForm.swift
@@ -1,13 +1,15 @@
 import UIKit
 
-class VOnboardForm:UIView
+class VOnboardForm:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:COnboardForm!
+    private weak var collectionView:UICollectionView!
+    private let kHeaderHeight:CGFloat = 100
+    private let kCollectionBottom:CGFloat = 50
     
     convenience init(controller:COnboardForm)
     {
         self.init()
-        alpha = 0
         clipsToBounds = true
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
@@ -19,10 +21,37 @@ class VOnboardForm:UIView
         visualEffect.clipsToBounds = true
         visualEffect.isUserInteractionEnabled = false
         
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize(width:0, height:kHeaderHeight)
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        flow.sectionInset = UIEdgeInsets(top:0, left:0, bottom:kCollectionBottom, right:0)
+        
+        let collectionView:UICollectionView = UICollectionView(frame:CGRect.zero, collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VOnboardFormHeader.self,
+            forSupplementaryViewOfKind:
+            UICollectionElementKindSectionHeader,
+            withReuseIdentifier:
+            VOnboardFormHeader.reusableIdentifier)
+        self.collectionView = collectionView
+        
         addSubview(visualEffect)
+        addSubview(collectionView)
         
         let views:[String:UIView] = [
-            "visualEffect":visualEffect]
+            "visualEffect":visualEffect,
+            "collectionView":collectionView]
         
         let metrics:[String:CGFloat] = [:]
         
@@ -33,6 +62,16 @@ class VOnboardForm:UIView
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"V:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[collectionView]-0-|",
             options:[],
             metrics:metrics,
             views:views))

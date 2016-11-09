32a53e2d6d50d99c6d8d9df086142a6dde6b5b7d
vauxhall <zero.griffin@gmail.com>
2016-11-09 13:30:02 -0600

    onboard form credentials

---
+++ nubecero/Model/OnboardForm/MOnboardFormCredentials.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+class MOnboardFormCredentials
+{
+    let email:String
+    let password:String
+    
+    init(email:String, password:String)
+    {
+        self.email = email
+        self.password = password
+    }
+}

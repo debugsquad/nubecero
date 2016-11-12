faff37a3787117e57e1afaac0ccdc2c61246252e
vauxhall <zero.griffin@gmail.com>
2016-11-12 15:52:43 -0600

    controller store

---
+++ nubecero/Controller/Store/CStore.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class CStore:CController
+{
+    weak var viewStore:VStore!
+    
+    override func loadView()
+    {
+        let viewStore:VStore = VStore(controller:self)
+        self.viewStore = viewStore
+        view = viewStore
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        MStore.sharedInstance.checkAvailability()
+    }
+}

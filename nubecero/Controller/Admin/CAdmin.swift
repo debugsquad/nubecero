c367dc667ee3c1c915581e8295f0517f28e95f1e
vauxhall <zero.griffin@gmail.com>
2016-11-12 17:02:10 -0600

    controller - view admin

--- nubecero/Controller/Admin/CAdmin.swift
+++ nubecero/Controller/Admin/CAdmin.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CAdmin:CController
 {
+    private weak var viewAdmin:VAdmin!
     
+    override func loadView()
+    {
+        let viewAdmin:VAdmin = VAdmin(controller:self)
+        self.viewAdmin = viewAdmin
+        view = viewAdmin
+    }
 }
--- nubecero/View/Admin/VAdmin.swift
+++ nubecero/View/Admin/VAdmin.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VAdmin:UIView
 {
+    private weak var controller:CAdmin!
     
+    convenience init(controller:CAdmin)
+    {
+        self.init()
+        backgroundColor = UIColor.background
+        clipsToBounds = true
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }

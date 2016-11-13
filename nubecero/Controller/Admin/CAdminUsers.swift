29ddf82ef3e84cb33288dd5db341a765ba167de6
vauxhall <zero.griffin@gmail.com>
2016-11-13 15:56:29 -0600

    users title

--- nubecero/Controller/Admin/CAdminUsers.swift
+++ nubecero/Controller/Admin/CAdminUsers.swift
@@ -4,6 +4,12 @@ class CAdminUsers:CController
 {
     private weak var viewUsers:VAdminUsers!
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        title = NSLocalizedString("CAdminUsers_title", comment:"")
+    }
+    
     override func loadView()
     {
         let viewUsers:VAdminUsers = VAdminUsers(controller:self)

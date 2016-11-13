e404a454ea871b774e7d5dbf92700a4aa104d80b
vauxhall <zero.griffin@gmail.com>
2016-11-13 15:24:19 -0600

    hover for admin cell

--- nubecero/View/Admin/VAdminCell.swift
+++ nubecero/View/Admin/VAdminCell.swift
@@ -2,13 +2,14 @@ import UIKit
 
 class VAdminCell:UICollectionViewCell
 {
-    weak var label:UILabel!
+    private weak var label:UILabel!
+    private let kAlphaSelected:CGFloat = 0.2
+    private let kAlphaNotSelected:CGFloat = 1
     
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor.white
         
         let label:UILabel = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -42,10 +43,43 @@ class VAdminCell:UICollectionViewCell
         fatalError()
     }
     
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            alpha = kAlphaSelected
+            backgroundColor = UIColor.clear
+        }
+        else
+        {
+            alpha = kAlphaNotSelected
+            backgroundColor = UIColor.white
+        }
+    }
+    
     //MARK: public
     
     func config(model:MAdminItem)
     {
         label.text = model.title
+        hover()
     }
 }

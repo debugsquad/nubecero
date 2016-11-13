a6dfce52702340b70a2d7091ab5c59dd6a91d070
vauxhall <zero.griffin@gmail.com>
2016-11-13 14:44:39 -0600

    added collection view to admin view

--- nubecero/View/Admin/VAdmin.swift
+++ nubecero/View/Admin/VAdmin.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-class VAdmin:UIView
+class VAdmin:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CAdmin!
+    private weak var collectionView:UICollectionView!
+    private let kInterLine:CGFloat = 1
+    private let kCollectionBottom:CGFloat = 20
     
     convenience init(controller:CAdmin)
     {
@@ -11,5 +14,49 @@ class VAdmin:UIView
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let barHeight:CGFloat = controller.parentController.viewParent.kBarHeight
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumInteritemSpacing = 0
+        flow.minimumLineSpacing = kInterLine
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        flow.sectionInset = UIEdgeInsets(top:kInterLine, left:0, bottom:kCollectionBottom, right:0)
+        
+        let collectionView:UICollectionView = UICollectionView(frame:CGRect.zero, collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VAdminCell.self,
+            forCellWithReuseIdentifier:
+            VAdminCell.reusableIdentifier)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
+        
+        let views:[String:UIView] = [
+            "collectionView":collectionView]
+        
+        let metrics:[String:CGFloat] = [
+            "barHeight":barHeight]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-(barHeight)-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }

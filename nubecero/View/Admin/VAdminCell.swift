e7baa26ef2c6792e242de3b1974ed11b7d68dc05
vauxhall <zero.griffin@gmail.com>
2016-11-13 15:14:24 -0600

    added label to admin cell

--- nubecero/View/Admin/VAdminCell.swift
+++ nubecero/View/Admin/VAdminCell.swift
@@ -2,11 +2,23 @@ import UIKit
 
 class VAdminCell:UICollectionViewCell
 {
+    weak var label:UILabel!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.white
+        
+        let label:UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.medium(size:15)
+        label.textColor = UIColor.black
+        label.isUserInteractionEnabled = false
+        self.label = label
+        
+        addSubview(label)
     }
     
     required init?(coder:NSCoder)

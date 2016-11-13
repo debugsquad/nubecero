f86d03f907f7319a15e0b2ea114af4884c1fb76e
vauxhall <zero.griffin@gmail.com>
2016-11-13 15:49:13 -0600

    controller -> view users

--- nubecero/Controller/Admin/CAdminUsers.swift
+++ nubecero/Controller/Admin/CAdminUsers.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CAdminUsers:CController
 {
+    private weak var viewUsers:VAdminUsers!
     
+    override func loadView()
+    {
+        let viewUsers:VAdminUsers = VAdminUsers(controller:self)
+        self.viewUsers = viewUsers
+        view = viewUsers
+    }
 }

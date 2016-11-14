c4bd1ae5bae507cd909998db2cbcc771cad05bf9
vauxhall <zero.griffin@gmail.com>
2016-11-14 15:39:48 -0600

    model item server froob

--- nubecero/Model/AdminServer/MAdminServerItemFroob.swift
+++ nubecero/Model/AdminServer/MAdminServerItemFroob.swift
@@ -1,6 +1,21 @@
-import Foundation
+import UIKit
 
 class MAdminServerItemFroob:MAdminServerItem
 {
+    private let kCellHeight:CGFloat = 70
+    private let kSelectable:Bool = false
     
+    override init()
+    {
+        let reusableIdentifier:String = ""
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            cellHeight:kCellHeight,
+            selectable:kSelectable)
+    }
+    
+    override init(reusableIdentifier:String, cellHeight:CGFloat, selectable:Bool)
+    {
+        fatalError()
+    }
 }

6e14b4863f5e3f63858f61a0b4f747a474137b6c
vauxhall <zero.griffin@gmail.com>
2016-11-14 12:11:07 -0600

    wording

--- nubecero/View/AdminUsers/VAdminUsersCell.swift
+++ nubecero/View/AdminUsers/VAdminUsersCell.swift
@@ -2,15 +2,26 @@ import UIKit
 
 class VAdminUsersCell:UICollectionViewCell
 {
+    private let dateFormatter:DateFormatter
+    private let numberFormatter:NumberFormatter
     private weak var labelUserId:UILabel!
     private weak var labelCreated:UILabel!
     private weak var labelLastSession:UILabel!
     private weak var labelDiskUsed:UILabel!
     private let kAlphaSelected:CGFloat = 0.2
     private let kAlphaNotSelected:CGFloat = 1
+    private let kKilobytesPerMega:CGFloat = 1000
+    private let kMaxFractions:Int = 3
     
     override init(frame:CGRect)
     {
+        dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        
+        numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = NumberFormatter.Style.decimal
+        numberFormatter.maximumFractionDigits = kMaxFractions
+        
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.white
@@ -43,7 +54,7 @@ class VAdminUsersCell:UICollectionViewCell
         labelDiskUsed.translatesAutoresizingMaskIntoConstraints = false
         labelDiskUsed.isUserInteractionEnabled = false
         labelDiskUsed.backgroundColor = UIColor.clear
-        labelDiskUsed.font = UIFont.numeric(size:18)
+        labelDiskUsed.font = UIFont.numeric(size:14)
         labelDiskUsed.textColor = UIColor.main
         self.labelDiskUsed = labelDiskUsed
         
@@ -81,7 +92,7 @@ class VAdminUsersCell:UICollectionViewCell
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-10-[labelUserId(18)]-0-[labelCreated(17)]-0-[labelLastSession(17)]-0-[labelDiskUsed(22)]",
+            withVisualFormat:"V:|-10-[labelUserId(18)]-2-[labelCreated(17)]-0-[labelLastSession(17)]-10-[labelDiskUsed(18)]",
             options:[],
             metrics:metrics,
             views:views))
@@ -129,5 +140,25 @@ class VAdminUsersCell:UICollectionViewCell
     func config(model:MAdminUsersItem)
     {
         labelUserId.text = model.userId
+        
+        let dateCreated:Date = Date(timeIntervalSince1970:model.created)
+        let dateLastSession:Date = Date(timeIntervalSince1970:model.lastSession)
+        let numberDiskUsed:NSNumber = (CGFloat(model.diskUsed) / kKilobytesPerMega) as NSNumber
+        let stringCreated:String = dateFormatter.string(from:dateCreated)
+        let stringLastSession:String = dateFormatter.string(from:dateLastSession)
+        let stringDiskUsed:String = numberFormatter.string(from:numberDiskUsed)!
+        let createdComposite:String = String(
+            format:NSLocalizedString("VAdminUsersCell_labelCreated", comment:""),
+            stringCreated)
+        let lastSessionComposite:String = String(
+            format:NSLocalizedString("VAdminUsersCell_labelLastSession", comment:""),
+            stringLastSession)
+        let diskUsedComposite:String = String(
+            format:NSLocalizedString("VAdminUsersCell_labelDiskUsed", comment:""),
+            stringDiskUsed)
+        
+        labelCreated.text = createdComposite
+        labelLastSession.text = lastSessionComposite
+        labelDiskUsed.text = diskUsedComposite
     }
 }

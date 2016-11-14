6c083262d250a18318445af669fd2f4f4b6248a1
vauxhall <zero.griffin@gmail.com>
2016-11-14 10:02:55 -0600

    init model user admin

--- nubecero/Model/AdminUsers/MAdminUsers.swift
+++ nubecero/Model/AdminUsers/MAdminUsers.swift
@@ -2,5 +2,22 @@ import Foundation
 
 class MAdminUsers
 {
+    let userList:FDatabaseModelUserList
+    var references:[MSession.UserId]
+    private let kDefaultOrder:Bool = false
     
+    init(userList:FDatabaseModelUserList)
+    {
+        self.userList = userList
+        references = []
+        
+        order(ascending:kDefaultOrder)
+    }
+    
+    //MARK: private
+    
+    private func order(ascending:Bool)
+    {
+        
+    }
 }

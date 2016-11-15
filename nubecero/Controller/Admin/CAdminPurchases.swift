69421c989c2fa6fde0282619bc839492bb1abf6f
vauxhall <zero.griffin@gmail.com>
2016-11-15 09:56:31 -0600

    controller purchases loads view

--- nubecero/Controller/Admin/CAdminPurchases.swift
+++ nubecero/Controller/Admin/CAdminPurchases.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CAdminPurchases:CController
 {
+    private weak var viewPurchases:VAdminPurchases!
     
+    override func loadView()
+    {
+        let viewPurchases:VAdminPurchases = VAdminPurchases(controller:self)
+        self.viewPurchases = viewPurchases
+        view = viewPurchases
+    }
 }

58c633c914218e3b1ac58fd68c6f88dbfd96b6e1
vauxhall <zero.griffin@gmail.com>
2016-11-15 11:47:12 -0600

    init model purchase item

--- nubecero/Model/AdminPurchases/MAdminPurchasesItem.swift
+++ nubecero/Model/AdminPurchases/MAdminPurchasesItem.swift
@@ -2,5 +2,14 @@ import Foundation
 
 class MAdminPurchasesItem
 {
+    var purchaseId:MStore.PurchaseId
+    var name:String
+    let created:TimeInterval
     
+    init(purchaseId:MStore.PurchaseId, firebasePurchase:FDatabaseModelPurchase)
+    {
+        self.purchaseId = purchaseId
+        name = firebasePurchase.name
+        created = firebasePurchase.created
+    }
 }

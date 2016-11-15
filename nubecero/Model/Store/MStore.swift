8498faf4a28a48e7e3fddbe94769928058b67bf8
vauxhall <zero.griffin@gmail.com>
2016-11-15 17:18:55 -0600

    load products and update transactions

--- nubecero/Model/Store/MStore.swift
+++ nubecero/Model/Store/MStore.swift
@@ -74,70 +74,70 @@ class MStore
     //MARK: public
     
     func loadSkProduct(skProduct:SKProduct)
-    {/*
-         let productId:String = skProduct.productIdentifier
-         
-         guard
-         
-         let mappedItem:MStorePurchaseItem = mapItems[productId]
-         
-         else
-         {
-         return
-         }
-         
-         mappedItem.skProduct = skProduct
-         priceFormatter.locale = skProduct.priceLocale
-         
-         let priceNumber:NSDecimalNumber = skProduct.price
-         let priceString:String? = priceFormatter.string(from:priceNumber)
-         mappedItem.price = priceString*/
+    {
+        let productId:String = skProduct.productIdentifier
+        
+        guard
+            
+            let mappedItem:MStoreItem = mapItems?[productId]
+            
+        else
+        {
+            return
+        }
+        
+        mappedItem.skProduct = skProduct
+        priceFormatter.locale = skProduct.priceLocale
+        
+        let priceNumber:NSDecimalNumber = skProduct.price
+        let priceString:String? = priceFormatter.string(from:priceNumber)
+        mappedItem.price = priceString
     }
     
     func updateTransactions(transactions:[SKPaymentTransaction])
-    {/*
-         for skPaymentTransaction:SKPaymentTransaction in transactions
-         {
-         let productId:String = skPaymentTransaction.payment.productIdentifier
-         
-         guard
-         
-         let mappedItem:MStorePurchaseItem = mapItems[productId]
-         
-         else
-         {
-         continue
-         }
-         
-         switch skPaymentTransaction.transactionState
-         {
-         case SKPaymentTransactionState.deferred:
-         
-         mappedItem.status = MStorePurchaseItemStatusDeferred()
-         
-         break
-         
-         case SKPaymentTransactionState.failed:
-         
-         mappedItem.status = MStorePurchaseItemStatusNew()
-         SKPaymentQueue.default().finishTransaction(skPaymentTransaction)
-         
-         break
-         
-         case SKPaymentTransactionState.purchased,
-         SKPaymentTransactionState.restored:
-         
-         mappedItem.purchased()
-         SKPaymentQueue.default().finishTransaction(skPaymentTransaction)
-         
-         break
-         
-         case SKPaymentTransactionState.purchasing:
-         
-         mappedItem.status = MStorePurchaseItemStatusPurchasing()
-         
-         break
-         }
-         }*/
+    {
+        for skPaymentTransaction:SKPaymentTransaction in transactions
+        {
+            let productId:String = skPaymentTransaction.payment.productIdentifier
+            
+            guard
+                
+                let mappedItem:MStoreItem = mapItems?[productId]
+                
+            else
+            {
+                continue
+            }
+            
+            switch skPaymentTransaction.transactionState
+            {
+                case SKPaymentTransactionState.deferred:
+                    
+                    mappedItem.statusDeferred()
+                    
+                    break
+                    
+                case SKPaymentTransactionState.failed:
+                    
+                    mappedItem.statusNew()
+                    SKPaymentQueue.default().finishTransaction(skPaymentTransaction)
+                    
+                    break
+                    
+                case SKPaymentTransactionState.purchased,
+                     SKPaymentTransactionState.restored:
+                    
+                    mappedItem.statusPurchased()
+                    SKPaymentQueue.default().finishTransaction(skPaymentTransaction)
+                    
+                    break
+                    
+                case SKPaymentTransactionState.purchasing:
+                    
+                    mappedItem.statusPurchasing()
+                    
+                    break
+            }
+        }
     }
 }

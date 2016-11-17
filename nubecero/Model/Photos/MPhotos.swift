026c7d7a5c539b84f03e18084547cd2c9a25d19e
vauxhall <zero.griffin@gmail.com>
2016-11-17 14:46:17 -0600

    model photos item

--- nubecero/Model/Photos/MPhotos.swift
+++ nubecero/Model/Photos/MPhotos.swift
@@ -2,4 +2,16 @@ import Foundation
 
 class MPhotos
 {
+    let items:[MPhotosItem]
+    
+    init()
+    {
+        let itemDefault:MPhotosItem = MPhotosItem(name:"default")
+        let itemWedding:MPhotosItem = MPhotosItem(name:"wedding")
+        
+        items = [
+            itemDefault,
+            itemWedding
+        ]
+    }
 }
---
+++ nubecero/Model/Photos/MPhotosItem.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+class MPhotosItem
+{
+    let name:String
+    
+    init(name:String)
+    {
+        self.name = name
+    }
+}

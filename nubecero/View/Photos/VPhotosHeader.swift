13d32736e0a413bacd3dc61c9e7d24d67b17514f
vauxhall <zero.griffin@gmail.com>
2016-11-17 16:28:14 -0600

    added button add to header

--- nubecero/View/Photos/VPhotosHeader.swift
+++ nubecero/View/Photos/VPhotosHeader.swift
@@ -8,7 +8,30 @@ class VPhotosHeader:UICollectionReusableView
     {
         super.init(frame:frame)
         clipsToBounds = true
-        backgroundColor = UIColor.white
+        backgroundColor = UIColor.clear
+        
+        let buttonAdd:UIButton = UIButton()
+        buttonAdd.translatesAutoresizingMaskIntoConstraints = false
+        buttonAdd.setImage(#imageLiteral(resourceName: "assetPhotosAlbumAdd"), for:UIControlState.normal)
+        buttonAdd.imageView!.clipsToBounds = true
+        buttonAdd.imageView!.contentMode = UIViewContentMode.center
+        addSubview(buttonAdd)
+        
+        let views:[String:UIView] = [
+            "buttonAdd":buttonAdd]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[buttonAdd(70)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[buttonAdd]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)

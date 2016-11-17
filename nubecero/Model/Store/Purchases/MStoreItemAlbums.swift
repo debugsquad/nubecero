58cc6c2330f6cee33bb7218608949909e41f6d45
vauxhall <zero.griffin@gmail.com>
2016-11-17 11:00:53 -0600

    wording

--- nubecero/Model/Store/Purchases/MStoreItemAlbums.swift
+++ nubecero/Model/Store/Purchases/MStoreItemAlbums.swift
@@ -7,8 +7,13 @@ class MStoreItemAlbums:MStoreItem
     override init()
     {
         let title:String = NSLocalizedString("MStoreItemAlbums_title", comment:"")
-        let title:String = NSLocalizedString("MStoreItemAlbums_title", comment:"")
-        let image:UIImage
+        let descr:String = NSLocalizedString("MStoreItemAlbums_descr", comment:"")
+        let image:UIImage = #imageLiteral(resourceName: "assetPurchaseAlbums")
+        super.init(
+            purchaseId:kStorePurchaseId,
+            title:title,
+            descr:descr,
+            image:image)
     }
     
     init(purchaseId:MStore.PurchaseId, title:String, descr:String, image:UIImage)

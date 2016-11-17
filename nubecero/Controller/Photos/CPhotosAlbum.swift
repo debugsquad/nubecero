4ff346c1b21acf68a1fb73bd2b383e3c012b8b07
vauxhall <zero.griffin@gmail.com>
2016-11-17 16:50:44 -0600

    controller view album

--- nubecero/Controller/Photos/CPhotosAlbum.swift
+++ nubecero/Controller/Photos/CPhotosAlbum.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CPhotosAlbum:CController
 {
+    private weak var viewAlbum:VPhotosAlbum!
     
+    override func loadView()
+    {
+        let viewAlbum:VPhotosAlbum = VPhotosAlbum(controller:self)
+        self.viewAlbum = viewAlbum
+        view = viewAlbum
+    }
 }

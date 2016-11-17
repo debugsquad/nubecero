a21dc92c5db1dfeab8ca105692649dc1c2604027
vauxhall <zero.griffin@gmail.com>
2016-11-17 15:16:10 -0600

    added album icon to cell

--- nubecero/View/Photos/VPhotosCell.swift
+++ nubecero/View/Photos/VPhotosCell.swift
@@ -11,28 +11,42 @@ class VPhotosCell:UICollectionViewCell
         super.init(frame:frame)
         clipsToBounds = true
         
+        let icon:UIImageView = UIImageView()
+        icon.isUserInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.center
+        icon.image = #imageLiteral(resourceName: "assetPhotosAlbum")
+        
         let labelName:UILabel = UILabel()
         labelName.isUserInteractionEnabled = false
         labelName.translatesAutoresizingMaskIntoConstraints = false
         labelName.backgroundColor = UIColor.clear
-        labelName.font = UIFont.regular(size:19)
-        labelName.textColor = UIColor(white:0.4, alpha:1)
+        labelName.font = UIFont.regular(size:18)
+        labelName.textColor = UIColor(white:0.5, alpha:1)
         self.labelName = labelName
         
         addSubview(labelName)
+        addSubview(icon)
         
         let views:[String:UIView] = [
-            "labelName":labelName]
+            "labelName":labelName,
+            "icon":icon]
         
         let metrics:[String:CGFloat] = [:]
         
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"H:|-10-[labelName]-10-|",
+            withVisualFormat:"H:|-[icon(50)]-[labelName]-10-|",
             options:[],
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-10-[labelName(20)]",
+            withVisualFormat:"V:|-10-[labelName(22)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[icon(42)]",
             options:[],
             metrics:metrics,
             views:views))

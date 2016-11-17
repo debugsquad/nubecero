a6c6a830fb8a1a35bb41af3e5d97429e1dd70455
vauxhall <zero.griffin@gmail.com>
2016-11-17 10:54:17 -0600

    model store item album

--- nubecero/Model/Store/Purchases/MStoreItemAlbums.swift
+++ nubecero/Model/Store/Purchases/MStoreItemAlbums.swift
@@ -1,1 +1,18 @@
-import Foundation
+import UIKit
+
+class MStoreItemAlbums:MStoreItem
+{
+    private let kStorePurchaseId:MStore.PurchaseId = "iturbide.nubercero.albums"
+    
+    override init()
+    {
+        let title:String = NSLocalizedString("MStoreItemAlbums_title", comment:"")
+        let title:String = NSLocalizedString("MStoreItemAlbums_title", comment:"")
+        let image:UIImage
+    }
+    
+    init(purchaseId:MStore.PurchaseId, title:String, descr:String, image:UIImage)
+    {
+        fatalError()
+    }
+}

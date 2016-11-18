0a795a329ac816f81f9ba642470dc8d1dbf13f02
vauxhall <zero.griffin@gmail.com>
2016-11-18 11:18:31 -0600

    model resources

--- nubecero/Model/Photos/MPhotosItemPhotoResources.swift
+++ nubecero/Model/Photos/MPhotosItemPhotoResources.swift
@@ -2,9 +2,157 @@ import UIKit
 
 class MPhotosItemPhotoResources
 {
+    private weak var item:MPhotosItemPhoto?
+    private weak var timer:Timer?
     private(set) var thumbnail:UIImage?
     private(set) var image:UIImage?
-    private weak var timer:Timer?
     private let kThumbnailSize:CGFloat = 128
     private let kPictureTimeout:TimeInterval = 15
+    
+    init(item:MPhotosItemPhoto?)
+    {
+        self.item = item
+    }
+    
+    @objc func timeoutCleanPicture(sender timer:Timer)
+    {
+        timer.invalidate()
+        cleanResources()
+    }
+    
+    //MARK: private
+    
+    private func imageDataReceived(data:Data)
+    {
+        guard
+            
+            let image:UIImage = UIImage(data:data)
+            
+        else
+        {
+            item?.stateClear()
+            
+            return
+        }
+        
+        self.image = image
+        countDown()
+        
+        NotificationCenter.default.post(
+            name:Notification.imageDataLoaded,
+            object:self)
+        
+        DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+            { [weak self] in
+                
+                if self?.thumbnail == nil
+                {
+                    self?.generateThumbnail()
+                }
+        }
+    }
+    
+    private func generateThumbnail()
+    {
+        guard
+            
+            let originalImage:UIImage = image
+            
+            else
+        {
+            stateClear()
+            
+            return
+        }
+        
+        let imageOriginalWidth:CGFloat = originalImage.size.width
+        let imageOriginalHeight:CGFloat = originalImage.size.height
+        let deltaWidth:CGFloat = imageOriginalWidth / kThumbnailSize
+        let deltaHeight:CGFloat = imageOriginalHeight / kThumbnailSize
+        let maxDelta:CGFloat = max(deltaWidth, deltaHeight)
+        let scaledWidth:CGFloat = floor(imageOriginalWidth / maxDelta)
+        let scaledHeight:CGFloat = floor(imageOriginalHeight / maxDelta)
+        let rect:CGRect = CGRect(x:0, y:0, width:scaledWidth, height:scaledHeight)
+        
+        UIGraphicsBeginImageContext(rect.size)
+        originalImage.draw(in:rect)
+        thumbnail = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        stateLoaded()
+        
+        NotificationCenter.default.post(
+            name:Notification.thumbnailReady,
+            object:self)
+    }
+    
+    private func asyncCountDown()
+    {
+        timer = Timer.scheduledTimer(
+            timeInterval:kPictureTimeout,
+            target:self,
+            selector:#selector(timeoutCleanPicture(sender:)),
+            userInfo:nil,
+            repeats:false)
+    }
+    
+    //MARK: public
+    
+    func cleanResources()
+    {
+        image = nil
+    }
+    
+    func loadImageData()
+    {
+        guard
+            
+            let userId:MSession.UserId = MSession.sharedInstance.userId
+            
+            else
+        {
+            return
+        }
+        
+        let parentUser:String = FStorage.Parent.user.rawValue
+        let imagePath:String = "\(parentUser)/\(userId)/\(pictureId)"
+        
+        FMain.sharedInstance.storage.loadData(
+            path:imagePath)
+        { [weak self] (data, error) in
+            
+            guard
+                
+                let dataStrong:Data = data
+                
+                else
+            {
+                self?.stateClear()
+                
+                return
+            }
+            
+            DispatchQueue.global(qos:DispatchQoS.QoSClass.background).async
+                { [weak self] in
+                    
+                    self?.imageDataReceived(data:dataStrong)
+            }
+        }
+    }
+    
+    func becameActive()
+    {
+        timer?.invalidate()
+    }
+    
+    func countDown()
+    {
+        timer?.invalidate()
+        
+        DispatchQueue.main.async
+            { [weak self] in
+                
+                self?.asyncCountDown()
+        }
+    }
 }

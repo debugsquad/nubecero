76cc624049ce5ffc04cbb636b3c515bfef626416
vauxhall <zero.griffin@gmail.com>
2016-11-18 10:02:19 -0600

    model photos item state

--- nubecero/Model/Photos/MPhotosItemState.swift
+++ nubecero/Model/Photos/MPhotosItemState.swift
@@ -1,9 +1,6 @@
-//
-//  MPhotosItemState.swift
-//  nubecero
-//
-//  Created by zero on 11/18/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MPhotosItemState
+{
+    
+}

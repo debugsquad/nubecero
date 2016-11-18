5998431b85ba0d23704e84ad06c739552244d3a6
vauxhall <zero.griffin@gmail.com>
2016-11-17 18:12:40 -0600

    added back button

--- nubecero/Controller/Photos/CPhotosAlbum.swift
+++ nubecero/Controller/Photos/CPhotosAlbum.swift
@@ -4,6 +4,18 @@ class CPhotosAlbum:CController
 {
     private weak var viewAlbum:VPhotosAlbum!
     
+    override func viewWillAppear(_ animated:Bool)
+    {
+        super.viewWillAppear(animated)
+        parentController.statusBarDefault()
+    }
+    
+    override func viewWillDisappear(_ animated:Bool)
+    {
+        super.viewWillDisappear(animated)
+        parentController.statusBarLight()
+    }
+    
     override func loadView()
     {
         let viewAlbum:VPhotosAlbum = VPhotosAlbum(controller:self)
--- nubecero/View/PhotosAlbum/VPhotosAlbum.swift
+++ nubecero/View/PhotosAlbum/VPhotosAlbum.swift
@@ -3,13 +3,55 @@ import UIKit
 class VPhotosAlbum:UIView
 {
     private weak var controller:CPhotosAlbum!
+    private weak var backButton:UIButton!
+    private weak var layoutBackgroundTop:NSLayoutConstraint!
+    private weak var layoutBackButtonTop:NSLayoutConstraint!
     
     convenience init(controller:CPhotosAlbum)
     {
         self.init()
         clipsToBounds = true
-        backgroundColor = UIColor.white
+        backgroundColor = UIColor.background
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let backButton:UIButton = UIButton()
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.setImage(
+            #imageLiteral(resourceName: "assetGenericBack").withRenderingMode(UIImageRenderingMode.alwaysTemplate),
+            for:UIControlState.normal)
+        backButton.imageView!.clipsToBounds = true
+        backButton.imageView!.contentMode = UIViewContentMode.center
+        backButton.imageView!.tintColor = UIColor.black
+        self.backButton = backButton
+        
+        addSubview(backButton)
+        
+        let views:[String:UIView] = [
+            "backButton":backButton]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[backButton(60)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:[backButton(44)]-",
+            options:[],
+            metrics:metrics,
+            views:views))
+        
+        layoutBackButtonTop = NSLayoutConstraint(
+            item:backButton,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:20)
+        
+        addConstraint(layoutBackButtonTop)
     }
 }

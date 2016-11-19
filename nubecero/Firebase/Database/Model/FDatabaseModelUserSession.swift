f9590a104919d2d7ea0dfd8a9bbaabb3166ffc19
vauxhall <zero.griffin@gmail.com>
2016-11-19 09:20:08 -0600

    firebase model session

---
+++ nubecero/Firebase/Database/Model/FDatabaseModelUserSession.swift
@@ -0,0 +1,118 @@
+import Foundation
+
+class FDatabaseModelUserSession:FDatabaseModel
+{
+    enum Property:String
+    {
+        case token = "token"
+        case timestamp = "timestamp"
+        case ttl = "ttl"
+        case version = "version"
+    }
+    
+    let token:String
+    let timestamp:TimeInterval
+    let ttl:Int
+    let version:String
+    private let kNoTime:TimeInterval = 0
+    private let kEmpty:String = ""
+    
+    init(email:String, status:Status)
+    {
+        self.email = email
+        self.status = status
+        token = kEmpty
+        created = NSDate().timeIntervalSince1970
+        lastSession = created
+        diskUsed = 0
+        
+        super.init()
+    }
+    
+    required init(snapshot:Any)
+    {
+        let snapshotDict:[String:Any]? = snapshot as? [String:Any]
+        
+        if let email:String = snapshotDict?[Property.email.rawValue] as? String
+        {
+            self.email = email
+        }
+        else
+        {
+            self.email = kEmpty
+        }
+        
+        if let token:String = snapshotDict?[Property.token.rawValue] as? String
+        {
+            self.token = token
+        }
+        else
+        {
+            self.token = kEmpty
+        }
+        
+        if let statusInt:Int = snapshotDict?[Property.status.rawValue] as? Int
+        {
+            if let status:Status = Status(rawValue:statusInt)
+            {
+                self.status = status
+            }
+            else
+            {
+                self.status = Status.unknown
+            }
+        }
+        else
+        {
+            self.status = Status.unknown
+        }
+        
+        if let created:TimeInterval = snapshotDict?[Property.created.rawValue] as? TimeInterval
+        {
+            self.created = created
+        }
+        else
+        {
+            self.created = kNoTime
+        }
+        
+        if let lastSession:TimeInterval = snapshotDict?[Property.lastSession.rawValue] as? TimeInterval
+        {
+            self.lastSession = lastSession
+        }
+        else
+        {
+            self.lastSession = kNoTime
+        }
+        
+        if let diskUsed:Int = snapshotDict?[Property.diskUsed.rawValue] as? Int
+        {
+            self.diskUsed = diskUsed
+        }
+        else
+        {
+            self.diskUsed = 0
+        }
+        
+        super.init()
+    }
+    
+    override init()
+    {
+        fatalError()
+    }
+    
+    override func modelJson() -> Any
+    {
+        let json:[String:Any] = [
+            Property.email.rawValue:email,
+            Property.token.rawValue:token,
+            Property.status.rawValue:status.rawValue,
+            Property.created.rawValue:created,
+            Property.lastSession.rawValue:lastSession,
+            Property.diskUsed.rawValue:diskUsed
+        ]
+        
+        return json
+    }
+}

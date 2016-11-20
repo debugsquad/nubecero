3c1dbbdfec57206716ccea14a74517ceb3523b79
vauxhall <zero.griffin@gmail.com>
2016-11-20 14:12:38 -0600

    button add for albums

--- nubecero/View/Photos/VPhotosHeaderAdd.swift
+++ nubecero/View/Photos/VPhotosHeaderAdd.swift
@@ -2,16 +2,74 @@ import UIKit
 
 class VPhotosHeaderAdd:UIButton
 {
-    private weak var controller:CPhotos!
+    private weak var header:VPhotosHeader!
+    private weak var image:UIImageView!
     
-    convenience init(controller:CPhotos)
+    convenience init(header:VPhotosHeader)
     {
         self.init()
         clipsToBounds = true
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
-        self.controller = controller
+        self.header = header
         
+        let image:UIImageView = UIImageView()
+        image.clipsToBounds = true
+        image.contentMode = UIViewContentMode.center
+        image.image = #imageLiteral(resourceName: "assetPhotosAlbumAdd").withRenderingMode(
+            UIImageRenderingMode.alwaysTemplate)
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.isUserInteractionEnabled = false
+        self.image = image
         
+        addSubview(image)
+        
+        let views:[String:UIView] = [
+            "image":image]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[image(50)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[image]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        
+        hover()
+    }
+    
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            image.tintColor = UIColor.main
+        }
+        else
+        {
+            image.tintColor = UIColor.complement
+        }
     }
 }

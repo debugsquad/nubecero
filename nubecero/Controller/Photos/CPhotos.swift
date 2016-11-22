3494a3d58ddf6058eb332d9829274ee9373f42ce
vauxhall <zero.griffin@gmail.com>
2016-11-22 17:01:28 -0600

    controller photos listens to photos loaded

--- nubecero/Controller/Photos/CPhotos.swift
+++ nubecero/Controller/Photos/CPhotos.swift
@@ -4,11 +4,34 @@ class CPhotos:CController
 {
     private weak var viewPhotos:VPhotos!
     
+    deinit
+    {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector:#selector(notifiedPhotosLoaded(sender:)),
+            name:Notification.photosLoaded,
+            object:nil)
+    }
+    
     override func loadView()
     {
         let viewPhotos:VPhotos = VPhotos(controller:self)
         self.viewPhotos = viewPhotos
         view = viewPhotos
+    }
+    
+    //MARK: notified
+    
+    func notifiedPhotosLoaded(sender notification:Notification)
+    {
+        
     }
     
     //MARK: public

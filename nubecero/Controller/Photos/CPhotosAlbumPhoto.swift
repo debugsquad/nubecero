303e36b63caa497a24fa0c08815e20bd33f64bdc
vauxhall <zero.griffin@gmail.com>
2016-11-23 17:19:45 -0600

    controller photo uses view photo

--- nubecero/Controller/Photos/CPhotosAlbumPhoto.swift
+++ nubecero/Controller/Photos/CPhotosAlbumPhoto.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CPhotosAlbumPhoto:CController
 {
+    private weak var viewPhoto:VPhotosAlbumPhoto!
     
+    override func loadView()
+    {
+        let viewPhoto:VPhotosAlbumPhoto = VPhotosAlbumPhoto(controller:self)
+        self.viewPhoto = viewPhoto
+        view = viewPhoto
+    }
 }
--- nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhoto.swift
+++ nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhoto.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class VPhotosAlbumPhoto:UIView
 {
+    private weak var controller:CPhotosAlbumPhoto!
     
+    convenience init(controller:CPhotosAlbumPhoto)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        translatesAutoresizingMaskIntoConstraints = false
+        self.controller = controller
+    }
 }

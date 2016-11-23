3111f696648dfd68202832e17ec7ba0b7aa40f82
vauxhall <zero.griffin@gmail.com>
2016-11-23 17:56:02 -0600

    background and visual effect on photo view

--- nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhoto.swift
+++ nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhoto.swift
@@ -3,6 +3,7 @@ import UIKit
 class VPhotosAlbumPhoto:UIView
 {
     private weak var controller:CPhotosAlbumPhoto!
+    private weak var imageView:UIImageView!
     
     convenience init(controller:CPhotosAlbumPhoto)
     {
@@ -11,5 +12,55 @@ class VPhotosAlbumPhoto:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.isUserInteractionEnabled = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        self.imageView = imageView
+        
+        let background:UIView = UIView()
+        background.isUserInteractionEnabled = false
+        background.translatesAutoresizingMaskIntoConstraints = false
+        background.clipsToBounds = true
+        background.alpha = 0
+        
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.dark)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.isUserInteractionEnabled = false
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        
+        background.addSubview(visualEffect)
+        addSubview(background)
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "background":background,
+            "visualEffect":visualEffect]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[background]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[background]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))x
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }

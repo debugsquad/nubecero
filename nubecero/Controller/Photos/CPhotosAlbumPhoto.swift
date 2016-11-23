609c94c861383afade6b5cc481113515188e9f21
vauxhall <zero.griffin@gmail.com>
2016-11-23 17:22:14 -0600

    controller photo init

--- nubecero/Controller/Photos/CPhotosAlbumPhoto.swift
+++ nubecero/Controller/Photos/CPhotosAlbumPhoto.swift
@@ -3,6 +3,18 @@ import UIKit
 class CPhotosAlbumPhoto:CController
 {
     private weak var viewPhoto:VPhotosAlbumPhoto!
+    weak var model:MPhotosItemPhoto!
+    
+    init(model:MPhotosItemPhoto, frame:CGRect)
+    {
+        self.model = model
+        super.init()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
     
     override func loadView()
     {

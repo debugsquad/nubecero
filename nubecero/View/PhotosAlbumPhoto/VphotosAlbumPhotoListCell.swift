1cb33c75b232a904e9a655f53fedb8863af6a0b6
vauxhall <zero.griffin@gmail.com>
2016-11-24 10:05:04 -0600

    list cell

--- nubecero/View/PhotosAlbumPhoto/VphotosAlbumPhotoListCell.swift
+++ nubecero/View/PhotosAlbumPhoto/VphotosAlbumPhotoListCell.swift
@@ -1,9 +1,3 @@
-//
-//  VphotosAlbumPhotoCell.swift
-//  nubecero
-//
-//  Created by zero on 11/24/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class VPhotosAlbumPhotoListCell:UICollectionViewCell

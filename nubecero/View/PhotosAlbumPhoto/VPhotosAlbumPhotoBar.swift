9eb4e6adfe5d48e4d48fca32c317505d9d717646
vauxhall <zero.griffin@gmail.com>
2016-11-25 10:51:35 -0600

    config bar cell

--- nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhotoBar.swift
+++ nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhotoBar.swift
@@ -3,7 +3,10 @@ import UIKit
 class VPhotosAlbumPhotoBar:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CPhotosAlbumPhoto!
+    private weak var collectionView:UICollectionView!
+    private weak var layoutCollectionWidth:NSLayoutConstraint!
     private let model:MPhotosAlbumPhoto
+    private let kCellWidth:CGFloat = 64
     
     init(controller:CPhotosAlbumPhoto)
     {
@@ -35,14 +38,47 @@ class VPhotosAlbumPhotoBar:UIView, UICollectionViewDelegate, UICollectionViewDat
         border.translatesAutoresizingMaskIntoConstraints = false
         border.clipsToBounds = true
         
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        flow.sectionInset = UIEdgeInsets.zero
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = false
+        collectionView.bounces = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VPhotosAlbumPhotoBarCell.self,
+            forCellWithReuseIdentifier:
+            VPhotosAlbumPhotoBarCell.reusableIdentifier)
+        self.collectionView = collectionView
+        addSubview(collectionView)
+        
         addSubview(border)
         addSubview(backButton)
+        addSubview(collectionView)
+        
+        let countCells:Int = model.items.count
+        let cellsWidth:CGFloat = CGFloat(countCells) * kCellWidth
         
         let views:[String:UIView] = [
             "backButton":backButton,
-            "border":border]
+            "border":border,
+            "collectionView":collectionView]
         
-        let metrics:[String:CGFloat] = [:]
+        let metrics:[String:CGFloat] = [
+            "cellsWidth":cellsWidth]
         
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[backButton(60)]",
@@ -51,6 +87,11 @@ class VPhotosAlbumPhotoBar:UIView, UICollectionViewDelegate, UICollectionViewDat
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
             withVisualFormat:"H:|-0-[border]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:[collectionView(cellsWidth)]-0-|",
             options:[],
             metrics:metrics,
             views:views))
@@ -64,6 +105,22 @@ class VPhotosAlbumPhotoBar:UIView, UICollectionViewDelegate, UICollectionViewDat
             options:[],
             metrics:metrics,
             views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-20-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        
+        layoutCollectionWidth = NSLayoutConstraint(
+            item:collectionView,
+            attribute:NSLayoutAttribute.width,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:nil,
+            attribute:NSLayoutAttribute.notAnAttribute,
+            multiplier:1,
+            constant:0)
+        
+        addConstraint(layoutCollectionWidth)
     }
     
     required init?(coder:NSCoder)
@@ -76,6 +133,15 @@ class VPhotosAlbumPhotoBar:UIView, UICollectionViewDelegate, UICollectionViewDat
     func actionBack(sender button:UIButton)
     {
         controller.back()
+    }
+    
+    //MARK: private
+    
+    private func modelAtIndex(index:IndexPath) -> MPhotosAlbumPhotoItem
+    {
+        let item:MPhotosAlbumPhotoItem = model.items[index.item]
+        
+        return item
     }
     
     //MARK: collectionView delegate
@@ -94,8 +160,13 @@ class VPhotosAlbumPhotoBar:UIView, UICollectionViewDelegate, UICollectionViewDat
     
     func collectionView(_ collectionView:UICollectionView, cellForItemAt indexPath:IndexPath) -> UICollectionViewCell
     {
+        let item:MPhotosAlbumPhotoItem = modelAtIndex(index:indexPath)
+        let cell:VPhotosAlbumPhotoBarCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier:
+            VPhotosAlbumPhotoBarCell.reusableIdentifier,
+            for:indexPath) as! VPhotosAlbumPhotoBarCell
+        cell.config(model:item)
         
-        
-        return UICollectionViewCell()
+        return cell
     }
 }

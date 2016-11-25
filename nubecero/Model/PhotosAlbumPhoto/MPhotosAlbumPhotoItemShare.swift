c7f71fee92b857c5a88601400bf71815a0968c6b
vauxhall <zero.griffin@gmail.com>
2016-11-25 10:01:18 -0600

    asset photos

--- nubecero/Model/PhotosAlbumPhoto/MPhotosAlbumPhotoItemShare.swift
+++ nubecero/Model/PhotosAlbumPhoto/MPhotosAlbumPhotoItemShare.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class MPhotosAlbumPhotoItemShare:MPhotosAlbumPhotoItem
 {
+    override init()
+    {
+        super.init(image:)
+    }
     
+    override init(image:UIImage)
+    {
+        fatalError()
+    }
 }

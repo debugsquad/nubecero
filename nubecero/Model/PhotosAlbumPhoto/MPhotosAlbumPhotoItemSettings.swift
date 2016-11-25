0d829d2a440776855a3c4d5ac622108db3024a83
vauxhall <zero.griffin@gmail.com>
2016-11-25 09:52:06 -0600

    factory model photos

--- nubecero/Model/PhotosAlbumPhoto/MPhotosAlbumPhotoItemSettings.swift
+++ nubecero/Model/PhotosAlbumPhoto/MPhotosAlbumPhotoItemSettings.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class MPhotosAlbumPhotoItemSettings:MPhotosAlbumPhotoItem
 {
+    override init()
+    {
+        super.init(image:)
+    }
     
+    override init(image:UIImage)
+    {
+        fatalError()
+    }
 }

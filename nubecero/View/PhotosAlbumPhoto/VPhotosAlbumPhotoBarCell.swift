fd28ab6a93e85ae85cf604057a177c5cf75c25cb
vauxhall <zero.griffin@gmail.com>
2016-11-25 10:21:54 -0600

    added image view to cell

--- nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhotoBarCell.swift
+++ nubecero/View/PhotosAlbumPhoto/VPhotosAlbumPhotoBarCell.swift
@@ -2,15 +2,49 @@ import UIKit
 
 class VPhotosAlbumPhotoBarCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let imageView:UIImageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.isUserInteractionEnabled = false
+        imageView.clipsToBounds = true
+        imageView.contentMode = UIViewContentMode.center
+        self.imageView = imageView
+        
+        addSubview(imageView)
+        
+        let views:[String:UIView] = [
+            "imageView":imageView]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[imageView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    //MARK: config
+    
+    func config(model:MPhotosAlbumPhotoItem)
+    {
+        imageView.image = model.image
     }
 }

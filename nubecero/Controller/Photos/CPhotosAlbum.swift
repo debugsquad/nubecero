0e52a016b9df5eafcebc590720a1d743ebdae152
vauxhall <zero.griffin@gmail.com>
2016-11-26 23:24:35 -0600

    alert controller for album options

--- nubecero/Controller/Photos/CPhotosAlbum.swift
+++ nubecero/Controller/Photos/CPhotosAlbum.swift
@@ -45,7 +45,32 @@ class CPhotosAlbum:CController
     
     func options()
     {
+        let alert:UIAlertController = UIAlertController(
+            title:
+            NSLocalizedString("CPhotosAlbumPhotoSettings_deleteTitle", comment:""),
+            message:
+            NSLocalizedString("CPhotosAlbumPhotoSettings_deleteMessage", comment:""),
+            preferredStyle:UIAlertControllerStyle.actionSheet)
         
+        let actionCancel:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("CPhotosAlbumPhotoSettings_deleteCancel", comment:""),
+            style:
+            UIAlertActionStyle.cancel)
+        
+        let actionDelete:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("CPhotosAlbumPhotoSettings_deleteDo", comment:""),
+            style:
+            UIAlertActionStyle.destructive)
+        { [weak self] (action:UIAlertAction) in
+            
+            self?.confirmedDelete()
+        }
+        
+        alert.addAction(actionDelete)
+        alert.addAction(actionCancel)
+        present(alert, animated:true, completion:nil)
     }
     
     func selectPhoto(item:Int, inRect:CGRect)

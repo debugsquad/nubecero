d020158b84473a1ee94dc44ab1405b5df81371e3
vauxhall <zero.griffin@gmail.com>
2016-11-27 02:49:06 -0600

    item status clouded

---
+++ nubecero/Model/HomeUpload/MHomeUploadItemStatusClouded.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class MHomeUploadItemStatusClouded:MHomeUploadItemStatus
+{
+    private let kAssetSync:String = "assetHomeSyncDone"
+    private let kFinished:Bool = false
+    private let kSelectable:Bool = false
+    
+    init(item:MHomeUploadItem?)
+    {
+        let reusableIdentifier:String = VHomeUploadCell.reusableIdentifier
+        let color:UIColor = UIColor.main
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            item:item,
+            assetSync:kAssetSync,
+            finished:kFinished,
+            selectable:kSelectable,
+            color:color)
+    }
+    
+    override init(
+        reusableIdentifier:String,
+        item:MHomeUploadItem?,
+        assetSync:String,
+        finished:Bool,
+        selectable:Bool,
+        color:UIColor)
+    {
+        fatalError()
+    }
+}

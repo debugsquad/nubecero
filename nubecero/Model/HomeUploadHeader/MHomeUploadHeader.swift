ef18a70115fc890186208711590001032c43c30a
vauxhall <zero.griffin@gmail.com>
2016-11-28 09:51:19 -0600

    home upload header item

--- nubecero/Model/HomeUploadHeader/MHomeUploadHeader.swift
+++ nubecero/Model/HomeUploadHeader/MHomeUploadHeader.swift
@@ -2,5 +2,11 @@ import Foundation
 
 class MHomeUploadHeader
 {
+    let items:[MHomeUploadHeaderItem]
     
+    init()
+    {
+        items = [
+        ]
+    }
 }
--- nubecero/Model/HomeUploadHeader/MHomeUploadHeaderItem.swift
+++ nubecero/Model/HomeUploadHeader/MHomeUploadHeaderItem.swift
@@ -1,9 +1,6 @@
-//
-//  MHomeUploadHeaderItem.swift
-//  nubecero
-//
-//  Created by zero on 11/28/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
-
 import Foundation
+
+class MHomeUploadHeaderItem
+{
+    
+}

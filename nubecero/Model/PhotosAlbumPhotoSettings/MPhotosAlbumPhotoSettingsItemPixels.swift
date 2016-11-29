ab2a48b5b6fd2f526ee9f410caf2ea698617af40
vauxhall <zero.griffin@gmail.com>
2016-11-29 13:19:31 -0600

    mode item pixels

---
+++ nubecero/Model/PhotosAlbumPhotoSettings/MPhotosAlbumPhotoSettingsItemPixels.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class MPhotosAlbumPhotoSettingsItemPixels:MPhotosAlbumPhotoSettingsItem
+{
+    private let kCellHeight:CGFloat = 150
+    private let kSelectable:Bool = false
+    
+    override init()
+    {
+        let reusableIdentifier:String = ""
+        
+        super.init(
+            reusableIdentifier:reusableIdentifier,
+            cellHeight:kCellHeight,
+            selectable:kSelectable)
+    }
+    
+    override init(
+        reusableIdentifier:String,
+        cellHeight:CGFloat,
+        selectable:Bool)
+    {
+        fatalError()
+    }
+}

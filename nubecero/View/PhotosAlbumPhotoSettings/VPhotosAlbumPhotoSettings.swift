4a08992a86a507171ab8d4c87a8375f37c0cf6e1
vauxhall <zero.griffin@gmail.com>
2016-11-29 13:29:53 -0600

    added collection view

--- nubecero/View/PhotosAlbumPhotoSettings/VPhotosAlbumPhotoSettings.swift
+++ nubecero/View/PhotosAlbumPhotoSettings/VPhotosAlbumPhotoSettings.swift
@@ -5,6 +5,8 @@ class VPhotosAlbumPhotoSettings:UIView, UICollectionViewDelegate, UICollectionVi
     private weak var controller:CPhotosAlbumPhotoSettings!
     private weak var collectionView:UICollectionView!
     private let kBarHeight:CGFloat = 120
+    private let kCollectionBottom:CGFloat = 20
+    private let kInterLine:CGFloat = 1
     
     convenience init(controller:CPhotosAlbumPhotoSettings)
     {
@@ -17,10 +19,41 @@ class VPhotosAlbumPhotoSettings:UIView, UICollectionViewDelegate, UICollectionVi
         let bar:VPhotosAlbumPhotoSettingsBar = VPhotosAlbumPhotoSettingsBar(
             controller:controller)
         
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = kInterLine
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        flow.sectionInset = UIEdgeInsets(
+            top:0,
+            left:0,
+            bottom:0,
+            right:kCollectionBottom)
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(
+            VPhotosAlbumPhotoSettingsCell.self,
+            forCellWithReuseIdentifier:
+            VPhotosAlbumPhotoSettingsCell.reusableIdentifier)
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
         addSubview(bar)
         
         let views:[String:UIView] = [
-            "bar":bar]
+            "bar":bar,
+            "collectionView":collectionView]
         
         let metrics:[String:CGFloat] = [
             "barHeight":kBarHeight]
@@ -31,7 +64,12 @@ class VPhotosAlbumPhotoSettings:UIView, UICollectionViewDelegate, UICollectionVi
             metrics:metrics,
             views:views))
         addConstraints(NSLayoutConstraint.constraints(
-            withVisualFormat:"V:|-0-[bar(barHeight)]",
+            withVisualFormat:"H:|-0-[collectionView]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[bar(barHeight)]-0-[collectionView]-0-|",
             options:[],
             metrics:metrics,
             views:views))

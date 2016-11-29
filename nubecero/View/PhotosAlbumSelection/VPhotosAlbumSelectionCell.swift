461e24cfcc6cac732cc91a88628026c868efce01
vauxhall <zero.griffin@gmail.com>
2016-11-28 18:58:45 -0600

    edit album selection

--- nubecero/View/PhotosAlbumSelection/VPhotosAlbumSelectionCell.swift
+++ nubecero/View/PhotosAlbumSelection/VPhotosAlbumSelectionCell.swift
@@ -2,5 +2,42 @@ import UIKit
 
 class VPhotosAlbumSelectionCell:UICollectionViewCell
 {
+    private weak var label:UILabel!
     
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.white
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.medium(size:16)
+        label.backgroundColor = UIColor.clear
+        self.label = label
+        
+        addSubview(label)
+        
+        let views:[String:UIView] = [
+            "label":label]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:[label]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
 }

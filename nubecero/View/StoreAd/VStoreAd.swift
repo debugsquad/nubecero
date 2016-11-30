d76cba4df658f781d4bca42d5f9dd3cca863dcfe
vauxhall <zero.griffin@gmail.com>
2016-11-30 09:41:38 -0600

    blurView on store ad

--- nubecero/View/StoreAd/VStoreAd.swift
+++ nubecero/View/StoreAd/VStoreAd.swift
@@ -2,5 +2,37 @@ import UIKit
 
 class VStoreAd:UIView
 {
+    private weak var controller:CStoreAd!
     
+    convenience init(controller:CStoreAd)
+    {
+        self.init()
+        clipsToBounds = true
+        backgroundColor = UIColor.clear
+        self.controller = controller
+        
+        let blurEffect:UIBlurEffect = UIBlurEffect(style:UIBlurEffectStyle.light)
+        let visualEffect:UIVisualEffectView = UIVisualEffectView(effect:blurEffect)
+        visualEffect.isUserInteractionEnabled = false
+        visualEffect.translatesAutoresizingMaskIntoConstraints = false
+        visualEffect.clipsToBounds = true
+        
+        addSubview(visualEffect)
+        
+        let views:[String:UIView] = [
+            "visualEffect":visualEffect]
+        
+        let metrics:[String:CGFloat] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"H:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraints(
+            withVisualFormat:"V:|-0-[visualEffect]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
 }

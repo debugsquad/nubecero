b0fcd2323632bdbd7dcda080560dd8ea8cab1d77
vauxhall <zero.griffin@gmail.com>
2016-11-30 09:51:29 -0600

    load view ad

--- nubecero/Controller/Store/CStoreAd.swift
+++ nubecero/Controller/Store/CStoreAd.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CStoreAd:CController
 {
+    private weak var viewAd:VStoreAd!
     
+    override func loadView()
+    {
+        let viewAd:VStoreAd = VStoreAd(controller:self)
+        self.viewAd = viewAd
+        view = viewAd
+    }
 }

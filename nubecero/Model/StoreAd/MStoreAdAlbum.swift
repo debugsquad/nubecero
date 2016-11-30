19fafb134b1ddfcabceb58d7ec5ff650f07392ac
vauxhall <zero.griffin@gmail.com>
2016-11-30 10:51:17 -0600

    models store ad

---
+++ nubecero/Model/StoreAd/MStoreAdAlbum.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MStoreAdAlbum:MStoreAd
+{
+    override init()
+    {
+        
+    }
+    
+    override init(image:UIImage, title:String, descr:String)
+    {
+        fatalError()
+    }
+}
---
+++ nubecero/Model/StoreAd/MStoreAdPlus.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MStoreAdPlus:MStoreAd
+{
+    override init()
+    {
+        
+    }
+    
+    override init(image:UIImage, title:String, descr:String)
+    {
+        fatalError()
+    }
+}
